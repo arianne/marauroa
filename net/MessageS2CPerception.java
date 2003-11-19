c929bf7a73f7c50a9b920efe2576565dd75f79f7
miguel <arianne_rpg@users.sourceforge.net>
2003-11-19 17:59:51 +0000

    no message

--- net/MessageS2CPerception.java
+++ net/MessageS2CPerception.java
@@ -2,10 +2,14 @@ package marauroa.net;
   
 import java.net.InetSocketAddress;
 import java.io.*;
+import java.util.*;
+
+import marauroa.game.*;
 
 public class MessageS2CPerception extends Message
-  {
-  /* TODO: Complete it */
+  {  
+  private List modifiedRPObjects;
+  private List deletedRPObjects;
   
   /** Constructor for allowing creation of an empty message */
   public MessageS2CPerception()
@@ -21,11 +25,13 @@ public class MessageS2CPerception extends Message
    *  @param 
    *  @see marauroa.net.MessageS2CCharacters
    */
-  public MessageS2CPerception(InetSocketAddress source,String[] characters)
+  public MessageS2CPerception(InetSocketAddress source,List modifiedRPObjects, List deletedRPObjects)
     {    
     super(source);
     
     type=TYPE_S2C_PERCEPTION;
+    this.modifiedRPObjects=modifiedRPObjects;
+    this.deletedRPObjects=deletedRPObjects;
     }  
   
 
@@ -37,11 +43,48 @@ public class MessageS2CPerception extends Message
   public void writeObject(marauroa.net.OutputSerializer out) throws IOException
     {
     super.writeObject(out);
+    
+    out.write(modifiedRPObjects.size());
+    
+    Iterator it_mod=modifiedRPObjects.iterator();
+    while(it_mod.hasNext())
+      {
+      out.write((RPObject)it_mod.next());
+      }
+
+    out.write(deletedRPObjects.size());
+
+    Iterator it_del=deletedRPObjects.iterator();
+    while(it_del.hasNext())
+      {
+      out.write((RPObject)it_del.next());
+      }
     }
     
   public void readObject(marauroa.net.InputSerializer in) throws IOException, java.lang.ClassNotFoundException
     {
     super.readObject(in);
+    
+    modifiedRPObjects=new LinkedList();
+    deletedRPObjects=new LinkedList();
+    
+    int mod=in.readInt();
+    
+    for(int i=0;i<mod;++i)
+      {
+      RPObject tmp=new RPObject(); 
+      tmp.readObject(in);
+      modifiedRPObjects.add(tmp);
+      }
+
+    int del=in.readInt();
+    
+    for(int i=0;i<del;++i)
+      {
+      RPObject tmp=new RPObject(); 
+      tmp.readObject(in);
+      deletedRPObjects.add(tmp);
+      }
     }    
  
   static private boolean registered=register();
