7cbff2b1357e7965e31d2a3bc9b180b7b9301823
miguel <arianne_rpg@users.sourceforge.net>
2003-11-21 14:16:15 +0000

    Added interface to PlayerDatabase thanks to root777

--- JMarauroa.java
+++ JMarauroa.java
@@ -1,247 +1,290 @@
+/**
+ * JMarauroa2.java
+ * derived from JMarauroa
+ * dont no if someone like it
+ * @author trwa@tribus.dyndns.org
+ */
+
 package marauroa;
 
-import java.awt.*;
-import java.applet.*;
 
-import marauroa.game.*;
 import marauroa.net.*;
-import java.io.*;
 
-public class JMarauroa extends javax.swing.JFrame {
-	private short clientid;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-    /** Creates new form fmarauroa */
-    public JMarauroa() {
-        initComponents();
-        clientid=-10;
+
+/**
+ * Test client for marauroa
+ **/
+public class JMarauroa2
+  extends JFrame
+{
+  
+  private JButton actionButton;
+  private JButton chooseCharacterButton;
+  private JButton connectButton;
+  private JButton disconnectButton;
+  private JTextArea reportsTextArea;
+  
+  private short clientId;
+  private NetworkClientManager netMan;
+  
+  private SimpleDateFormat formatter;
+  private Date logDate;
+  
+  
+  
+  public JMarauroa2()
+  {
+    setTitle("Marauroa test client");
+    //    setIconImage(new ImageIcon("marauroa.png").getImage());
+    initComponents();
+    clientId=-10;
+    formatter = new SimpleDateFormat("[HH:MM:ss.SSS]  ");
+    logDate = new Date();
+    addWindowListener(new WindowAdapter()
+                      {
+          public void windowClosing(WindowEvent p0)
+          {
+            exitForm(p0);
+          }
+        });
+  }
+  
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String args[])
+  {
+    JMarauroa2 marauroa = new JMarauroa2();
+    marauroa.pack();
+    marauroa.show();
+  }
+  
+  
+  /** Exit the Application */
+  private void exitForm(WindowEvent evt)
+  {
+    if(disconnectButton.isEnabled())
+    {
+      disconnectButton.doClick();
+      // ;-)
     }
-
+    System.exit(0);
+  }
+  
+  
+  /**
+   * This method is called from within the init() method to
+   * initialize the form.
+   */
+  private void initComponents()
+  {
+    JPanel main_panel = new JPanel(new BorderLayout());
+    reportsTextArea = new JTextArea();
     
-    /** Exit the Application */
-    private void exitForm(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_exitForm
-        System.exit(0);
-    }//GEN-LAST:event_exitForm
+    connectButton = new JButton("Connect");
+    connectButton.setMnemonic('C');
+    chooseCharacterButton = new JButton("Character");
+    chooseCharacterButton.setMnemonic('h');
+    actionButton = new JButton("Action");
+    actionButton.setMnemonic('A');
+    disconnectButton = new JButton("Disconnect");
+    disconnectButton.setMnemonic('D');
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        new JMarauroa().show();
+    reportsTextArea.setEditable(false);
+    reportsTextArea.setLineWrap(false);
+    
+    connectButton.addActionListener(new ActionListener()
+                                    {
+          public void actionPerformed(ActionEvent evt)
+          {
+            connectButtonActionPerformed(evt);
+          }
+        });
+    
+    chooseCharacterButton.setEnabled(false);
+    chooseCharacterButton.addActionListener(new ActionListener()
+                                            {
+          public void actionPerformed(ActionEvent evt)
+          {
+            chooseCharacterButtonActionPerformed(evt);
+          }
+        });
+    
+    actionButton.setEnabled(false);
+    actionButton.addActionListener(new ActionListener()
+                                   {
+          public void actionPerformed(ActionEvent evt)
+          {
+            actionButtonActionPerformed(evt);
+          }
+        });
+    
+    disconnectButton.setEnabled(false);
+    disconnectButton.addActionListener(new ActionListener()
+                                       {
+          public void actionPerformed(ActionEvent evt)
+          {
+            disconnectButtonActionPerformed(evt);
+          }
+        });
+    
+    JScrollPane sp = new JScrollPane(reportsTextArea);
+    sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    main_panel.add(sp,BorderLayout.CENTER);
+    
+    JPanel btn_panel = new JPanel(new FlowLayout());
+    btn_panel.add(connectButton);
+    btn_panel.add(chooseCharacterButton);
+    btn_panel.add(actionButton);
+    btn_panel.add(disconnectButton);
+    
+    main_panel.add(btn_panel,BorderLayout.SOUTH);
+    main_panel.setPreferredSize(new Dimension(580, 200));
+    main_panel.setMinimumSize(new Dimension(580, 200));
+    
+    setContentPane(main_panel);
+  }
+  
+  private void disconnectButtonActionPerformed(ActionEvent evt)
+  {
+    /* TODO: Write here the code to send Logout message */
+    Message msg=new MessageC2SLogout(null);
+    msg.setClientID(clientId);
+    netMan.addMessage(msg);
+    netMan.finish();
+    
+    connectButton.setEnabled(true);
+    chooseCharacterButton.setEnabled(false);
+    actionButton.setEnabled(false);
+    disconnectButton.setEnabled(false);
+  }
+  
+  private void actionButtonActionPerformed(ActionEvent evt)
+  {
+    /* TODO: Write here the code to send Action message */
+  }
+  
+  private void chooseCharacterButtonActionPerformed(ActionEvent evt)
+  {
+    /* TODO: Write here the code to send Choose Character message */
+    addLog("MrBean choosen\n");
+    Message msg=new MessageC2SChooseCharacter(null,"MrBean");
+    msg.setClientID(clientId);
+    
+    netMan.addMessage(msg);
+    
+    Message msgReply=null;
+    while(msgReply==null)
+    {
+      msgReply=netMan.getMessage();
+      
+      if(msgReply==null) continue;
+      
+      if(msgReply.getType()==Message.TYPE_S2C_CHOOSECHARACTER_ACK)
+      {
+        addLog("Character choosen correctly\n");
+      }
+      
+      if(msgReply.getType()==Message.TYPE_S2C_CHOOSECHARACTER_NACK)
+      {
+        addLog("Character not choosen. Error.\n");
+      }
+    }
+    chooseCharacterButton.setEnabled(false);
+    actionButton.setEnabled(true);
+  }
+  
+  private void connectButtonActionPerformed(ActionEvent evt)
+  {
+    /* Todo: Write here the code to send Login message */
+    addLog("Starting network client manager\n");
+    try
+    {
+      netMan=new NetworkClientManager("192.168.100.100");
+    }
+    catch(java.net.SocketException e)
+    {
+      ByteArrayOutputStream out= new ByteArrayOutputStream();
+      e.printStackTrace(new PrintStream(out));
+      
+      addLog("Exception starting network client manager\n");
+      addLog(out.toString()+"\n");
     }
     
-
-    private NetworkClientManager netMan;
+    MessageC2SLogin msg=new MessageC2SLogin(null,"Test Player","Test Password");
+    netMan.addMessage(msg);
     
-    /** Initializes the applet jmarauroa */
-    public void init() {
-   
-        initComponents();
-          
+    boolean is_correct_login=false;
+    Message msgReply=null;
+    while(msgReply==null)
+    {
+      msgReply=netMan.getMessage();
+      
+      if(msgReply==null) continue;
+      
+      if(msgReply.getType()==Message.TYPE_S2C_LOGIN_NACK)
+      {
+        addLog("Login reject because "+((MessageS2CLoginNACK)msgReply).getResolution()+"\n");
+      }
+      
+      if(msgReply.getType()==Message.TYPE_S2C_LOGIN_ACK)
+      {
+        addLog("Login successful\n");
+        clientId=msgReply.getClientID();
+        addLog("Recieved clientid: "+clientId+"\n");
+        is_correct_login=true;
+      }
     }
     
-    /** This method is called from within the init() method to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
-     */
-    private void initComponents() {//GEN-BEGIN:initComponents
-        titleLabel = new javax.swing.JLabel();
-        reportsPanel = new javax.swing.JPanel();
-        reportsTextArea = new javax.swing.JTextArea();
-        connectButton = new javax.swing.JButton();
-        chooseCharacterButton = new javax.swing.JButton();
-        actionButton = new javax.swing.JButton();
-        disconnectButton = new javax.swing.JButton();
-
-        titleLabel.setText("Marauroa test client");
-        titleLabel.setMinimumSize(new java.awt.Dimension(114, 32));
-        titleLabel.setPreferredSize(new java.awt.Dimension(114, 32));
-        titleLabel.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(titleLabel, java.awt.BorderLayout.NORTH);
-
-        reportsPanel.setBorder(new javax.swing.border.BevelBorder(javax.swing.border.BevelBorder.RAISED));
-        reportsPanel.setMaximumSize(new java.awt.Dimension(32767, 300));
-        reportsPanel.setMinimumSize(new java.awt.Dimension(600, 300));
-        reportsPanel.setPreferredSize(new java.awt.Dimension(600, 300));
-        reportsTextArea.setEditable(false);
-        reportsTextArea.setLineWrap(true);
-        //reportsTextArea.setFont(new java.awt.Font("Arial", 0, 10));
-        reportsTextArea.setMinimumSize(new java.awt.Dimension(580, 200));
-        reportsTextArea.setPreferredSize(new java.awt.Dimension(580, 200));
-        reportsTextArea.setAutoscrolls(true);
-        reportsPanel.add(reportsTextArea);
-
-        connectButton.setText("Connect");
-        connectButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                connectButtonActionPerformed(evt);
-            }
-        });
-
-        reportsPanel.add(connectButton);
-
-        chooseCharacterButton.setText("Character");
-        chooseCharacterButton.setEnabled(false);
-        chooseCharacterButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                chooseCharacterButtonActionPerformed(evt);
-            }
-        });
-
-        reportsPanel.add(chooseCharacterButton);
-
-        actionButton.setText("Action");
-        actionButton.setEnabled(false);
-        actionButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                actionButtonActionPerformed(evt);
-            }
-        });
-
-        reportsPanel.add(actionButton);
-
-        disconnectButton.setText("Disconnect");
-        disconnectButton.setEnabled(false);
-        disconnectButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                disconnectButtonActionPerformed(evt);
-            }
-        });
-
-        reportsPanel.add(disconnectButton);
-
-        getContentPane().add(reportsPanel, java.awt.BorderLayout.WEST);
-        pack();
-    }//GEN-END:initComponents
-
-    private void disconnectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_disconnectButtonActionPerformed
-        // Add your handling code here:
-        /* TODO: Write here the code to send Logout message */
-        Message msg=new MessageC2SLogout(null);
-        msg.setClientID(clientid);
-        netMan.addMessage(msg);
-        netMan.finish();
-        
-        connectButton.setEnabled(true);
-        chooseCharacterButton.setEnabled(false);
-        actionButton.setEnabled(false);
-        disconnectButton.setEnabled(false);
-    }//GEN-LAST:event_disconnectButtonActionPerformed
-
-    private void actionButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_actionButtonActionPerformed
-        // Add your handling code here:
-       /* TODO: Write here the code to send Action message */
-    }//GEN-LAST:event_actionButtonActionPerformed
-
-    private void chooseCharacterButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chooseCharacterButtonActionPerformed
-        // Add your handling code here:
-        /* TODO: Write here the code to send Choose Character message */
-        reportsTextArea.append("\n"+"MrBean choosen");
-        Message msg=new MessageC2SChooseCharacter(null,"MrBean");
-        msg.setClientID(clientid);
-        
-        netMan.addMessage(msg);
-        
-        Message msgReply=null;
-        while(msgReply==null)
-          {
-          msgReply=netMan.getMessage();
-          
-          if(msgReply==null) continue;
-          
-          if(msgReply.getType()==Message.TYPE_S2C_CHOOSECHARACTER_ACK)
-            {
-            reportsTextArea.append("\n"+"Character choosen correctly");
-            }
-
-          if(msgReply.getType()==Message.TYPE_S2C_CHOOSECHARACTER_NACK)
-            {
-            reportsTextArea.append("\n"+"Character not choosen. Error.");
-            }
-          }
-            
-        
-        chooseCharacterButton.setEnabled(false);
-        actionButton.setEnabled(true);
-    }//GEN-LAST:event_chooseCharacterButtonActionPerformed
-
-    private void connectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connectButtonActionPerformed
-        // Add your handling code here:
-        /* Todo: Write here the code to send Login message */
-        reportsTextArea.append("\n"+"Starting network client manager");
-        try
-          {
-          netMan=new NetworkClientManager("127.0.0.1");
-          }
-        catch(java.net.SocketException e)
-          {
-          ByteArrayOutputStream out= new ByteArrayOutputStream();
-          e.printStackTrace(new PrintStream(out));
-                   
-          reportsTextArea.append("\n"+"Exception starting network client manager");
-          reportsTextArea.append("\n"+out.toString());          
-          }
-          
-        MessageC2SLogin msg=new MessageC2SLogin(null,"Test Player","Test Password");
-        netMan.addMessage(msg);
-        
-        boolean LoginCorrect=false;
-        Message msgReply=null;
-        while(msgReply==null)
-          {
-          msgReply=netMan.getMessage();
-          
-          if(msgReply==null) continue;
-          
-          if(msgReply.getType()==Message.TYPE_S2C_LOGIN_NACK)
-            {
-            reportsTextArea.append("\n"+"Login reject because "+((MessageS2CLoginNACK)msgReply).getResolution());
-            }
-            
-          if(msgReply.getType()==Message.TYPE_S2C_LOGIN_ACK)
-            {
-            reportsTextArea.append("\n"+"Login successful");
-            clientid=msgReply.getClientID();
-            System.out.println("Recieved clientid: "+clientid);
-            LoginCorrect=true;
-            }          
-          }
-
-	    msgReply=null;
-        while(LoginCorrect && (msgReply==null))
-          {
-          msgReply=netMan.getMessage();
-
-          if(msgReply==null) continue;
-
-          if(msgReply.getType()==Message.TYPE_S2C_CHARACTERLIST)
-            {
-            reportsTextArea.append("\n"+"Character list received");
-            String[] characters=((MessageS2CCharacterList)msgReply).getCharacters();
-            for(int i=0;i<characters.length;++i)
-              {
-              reportsTextArea.append("\n- "+characters[i]);
-              }
-            }          
-          }
-        
-        disconnectButton.setEnabled(true);
-        chooseCharacterButton.setEnabled(true);
-        connectButton.setEnabled(false);
-    }//GEN-LAST:event_connectButtonActionPerformed
-
-    public void destroy() {
-    }    
-    
-    
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton actionButton;
-    private javax.swing.JButton chooseCharacterButton;
-    private javax.swing.JButton connectButton;
-    private javax.swing.JButton disconnectButton;
-    private javax.swing.JPanel reportsPanel;
-    private javax.swing.JTextArea reportsTextArea;
-    private javax.swing.JLabel titleLabel;
-    // End of variables declaration//GEN-END:variables
-    
+    msgReply=null;
+    while(is_correct_login && (msgReply==null))
+    {
+      msgReply=netMan.getMessage();
+      
+      if(msgReply==null) continue;
+      
+      if(msgReply.getType()==Message.TYPE_S2C_CHARACTERLIST)
+      {
+        addLog("Character list received\n");
+        String[] characters=((MessageS2CCharacterList)msgReply).getCharacters();
+        for(int i=0;i<characters.length;++i)
+        {
+          addLog("- "+characters[i]+"\n");
+        }
+      }
+    }
+    disconnectButton.setEnabled(true);
+    chooseCharacterButton.setEnabled(true);
+    connectButton.setEnabled(false);
+  }
+  
+  /**
+   * adds a message into reportPane
+   */
+  private void addLog(String msg)
+  {
+    logDate.setTime(System.currentTimeMillis());
+    String line = formatter.format(logDate) + msg;
+    reportsTextArea.append(line);
+    reportsTextArea.setCaretPosition(reportsTextArea.getText().length());
+  }
+  
 }
+
---
+++ game/MemoryPlayerDatabase.java
@@ -0,0 +1,291 @@
+package marauroa.game;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import marauroa.marauroad;
+
+public class MemoryPlayerDatabase implements PlayerDatabase
+  {
+  private final static byte MAX_NUMBER_OF_LOGIN_EVENTS=5;
+  private static PlayerDatabase playerDatabase;
+  
+  static class LoginEvent
+    {
+    public String address;
+    public Date time;
+    public boolean correct;
+    
+    public String toString()
+      {
+      return "Login "+(correct?"SUCESSFULL":"FAILED")+" at "+time.toString()+" from "+address;
+      }
+    }  
+
+  static class RPCharacter
+    {
+    public String character;
+    public RPObject object;
+    }
+    
+  static class PlayerEntry
+    {
+    public String username;
+    public String password;
+    public HashMap characters;
+    public List lastLogins;
+ 
+    public PlayerEntry()
+      {
+      characters=new HashMap();
+      lastLogins=new LinkedList();
+      }
+    }
+  
+  private Map players;
+  
+  private MemoryPlayerDatabase()
+    {
+    players=new HashMap();
+    }
+    
+  public static PlayerDatabase getDatabase()
+    {
+    if(playerDatabase==null)
+      {
+      playerDatabase=new MemoryPlayerDatabase();
+      }
+      
+    return playerDatabase;
+    }
+  
+  public int getPlayerCount()
+    {
+    return players.size();
+    }
+    
+  public boolean verifyAccount(String username, String password)
+    {
+    marauroad.trace("MemoryPlayerDatabase::verifyAccount",">");
+    if(players.containsKey(username))
+      {
+      PlayerEntry player=(PlayerEntry)players.get(username);
+      if(player.password.equals(password))
+        {
+        marauroad.trace("MemoryPlayerDatabase::verifyAccount","I","Username-password correct");
+        marauroad.trace("MemoryPlayerDatabase::verifyAccount","<");
+        return true;
+        }
+      else
+        {
+        marauroad.trace("MemoryPlayerDatabase::verifyAccount","I","Username-password incorrect");
+        marauroad.trace("MemoryPlayerDatabase::verifyAccount","<");
+        return false;
+        }
+      }
+
+    marauroad.trace("MemoryPlayerDatabase::verifyAccount","<");
+    return false;
+    }
+  
+  public void addLoginEvent(String username,InetSocketAddress source, boolean correctLogin) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::addLoginEvent",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::addLoginEvent","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    LoginEvent loginEvent=new LoginEvent();
+    loginEvent.address=source.toString();
+    loginEvent.time=new Date();
+    loginEvent.correct=correctLogin;
+    
+    player.lastLogins.add(loginEvent);
+    
+    if(player.lastLogins.size()>MAX_NUMBER_OF_LOGIN_EVENTS)
+      {
+      player.lastLogins.remove(0);
+      }
+      
+    marauroad.trace("MemoryPlayerDatabase::addLoginEvent","<");
+    }
+
+  public String[] getLoginEvent(String username) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::getLoginEvent",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::getLoginEvent","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    String[] events=new String[player.lastLogins.size()];
+    
+    Iterator it=player.lastLogins.iterator();
+    
+    int i=0;
+    while(it.hasNext())
+      {
+      LoginEvent entry=(LoginEvent)it.next();
+      events[i]=entry.toString();
+      }
+      
+    marauroad.trace("MemoryPlayerDatabase::getLoginEvent","<");
+    return events;
+    }
+    
+  public String[] getCharactersList(String username) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::getCharactersList",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::getCharactersList","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    String[] characters=new String[player.characters.size()];
+    
+    Iterator it=player.characters.entrySet().iterator();
+    
+    int i=0;
+    while(it.hasNext())
+      {
+      Map.Entry entry=(Map.Entry)it.next();
+      characters[i]=(String)entry.getKey();
+      ++i;
+      }
+      
+    marauroad.trace("MemoryPlayerDatabase::getCharactersList","<");
+    return characters;
+    }
+    
+  public void setRPObject(String username,String character, RPObject object) throws PlayerNotFoundException, CharacterNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::setRPObject",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::setRPObject","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    
+    if(!player.characters.containsKey(character))
+      {
+      marauroad.trace("MemoryPlayerDatabase::setRPObject","E","Player("+username+") doesn't contains that character("+character+")");
+      throw new CharacterNotFoundException();
+      }
+      
+    player.characters.put(character,object);
+    marauroad.trace("MemoryPlayerDatabase::setRPObject","<");
+    }
+    
+  public RPObject getRPObject(String username,String character) throws PlayerNotFoundException, CharacterNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::getRPObject",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::getRPObject","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    
+    if(!player.characters.containsKey(character))
+      {
+      marauroad.trace("MemoryPlayerDatabase::getRPObject","E","Player("+username+") doesn't contains that character("+character+")");
+      throw new CharacterNotFoundException();
+      }
+      
+    marauroad.trace("MemoryPlayerDatabase::getRPObject","<");
+    return (RPObject)player.characters.get(character);
+    }
+    
+  public void addPlayer(String username, String password) throws PlayerAlreadyAddedException
+    {
+    marauroad.trace("MemoryPlayerDatabase::addPlayer",">");
+    if(players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::addPlayer","E","Database already contains that username("+username+")");
+      throw new PlayerAlreadyAddedException();
+      }
+      
+    PlayerEntry player=new PlayerEntry();
+    player.password=password;
+    
+    players.put(username,player);
+    marauroad.trace("MemoryPlayerDatabase::addPlayer","<");
+    }
+  
+  public void removePlayer(String username) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::removePlayer",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::removePlayer","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    players.remove(username);
+    marauroad.trace("MemoryPlayerDatabase::removePlayer","<");
+    }
+
+  public boolean hasCharacter(String username, String character) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::hasCharacter",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::hasCharacter","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    marauroad.trace("MemoryPlayerDatabase::hasCharacter","<");
+    return player.characters.containsKey(character);
+    }
+    
+  public void addCharacter(String username, String character, RPObject object) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::addCharacter",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::addCharacter","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    player.characters.put(character,object);
+    marauroad.trace("MemoryPlayerDatabase::addCharacter","<");
+    }
+    
+  public void removeCharacter(String username, String character) throws PlayerNotFoundException
+    {
+    marauroad.trace("MemoryPlayerDatabase::removeCharacter",">");
+    if(!players.containsKey(username))
+      {
+      marauroad.trace("MemoryPlayerDatabase::removeCharacter","E","Database doesn't contains that username("+username+")");
+      throw new PlayerNotFoundException();
+      }
+      
+    PlayerEntry player=(PlayerEntry)players.get(username);
+    player.characters.remove(character);
+    marauroad.trace("MemoryPlayerDatabase::removeCharacter","<");
+    }
+    
+  public boolean hasPlayer(String username)
+    {
+    if(players.containsKey(username))
+      {
+      return true;
+      }
+    else
+      {
+      return false;
+      }      
+    }
+  }
