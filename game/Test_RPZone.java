ee2f671dd2f50483fa07ab847f0c0bab08d62147
root <root777@users.sourceforge.net>
2003-11-28 21:26:06 +0000

    changes because of changes in RPZone and RPRuleProcessor

--- game/Test_RPZone.java
+++ game/Test_RPZone.java
@@ -5,44 +5,44 @@ import marauroa.net.*;
 import java.io.*;
 
 public class Test_RPZone extends TestCase
+{
+  public static Test suite ( )
   {
-  public static Test suite ( ) 
-    {
     return new TestSuite(Test_RPZone.class);
-	}
-	
+  }
+  
   public void testRPZone()
-    {
+  {
     RPObject SonGoku=new RPObject();
     SonGoku.put("object_id","1");
     SonGoku.put("name","Son Goku");
-
-    RPZone zone=new RPZone();
+    
+    RPZone zone=new MarauroaRPZone();
     assertNotNull(zone);
     
     try
-      {
+    {
       zone.add(SonGoku);
       RPObject.ID id=new RPObject.ID(SonGoku);
-      assertTrue(zone.has(id));   
+      assertTrue(zone.has(id));
       
       RPObject object=zone.get(id);
       assertEquals(object,SonGoku);
       
       zone.remove(id);
       assertFalse(zone.has(id));
-      }
+    }
     catch(RPZone.RPObjectInvalidException e)
-      {
+    {
       fail("RPObject is not valid");
-      }
+    }
     catch(RPZone.RPObjectNotFoundException e)
-      {
+    {
       fail("RPObject doesn't exist");
-      }
+    }
     catch(Attributes.AttributeNotFoundException e)
-      {
+    {
       fail("Can't find the attribute we are looking for");
-      }    
     }
   }
+}
