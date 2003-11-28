9a7d970b158fd4bc10969de1ab858a7cc6ee5cc1
root <root777@users.sourceforge.net>
2003-11-28 21:24:47 +0000

    interface implementations

---
+++ game/MarauroaRPRuleProcessor.java
@@ -0,0 +1,32 @@
+/**
+ * MarauroaRPRuleProcessor.java
+ *
+ * @author Created by wt
+ */
+
+package marauroa.game;
+
+import java.util.LinkedList;
+import marauroa.marauroad;
+
+public class MarauroaRPRuleProcessor
+  implements RPRuleProcessor
+{
+  private RPZone zone;
+  
+  public MarauroaRPRuleProcessor(RPZone zone)
+  {
+    this.zone=zone;
+  }
+  
+  public RPAction.Status execute(RPObject.ID id, LinkedList list)
+  {
+    marauroad.trace("RPRuleProcessor::execute",">");
+    /* TODO: Implement action procession code */
+    marauroad.trace("RPRuleProcessor::execute","<");
+    
+    return new RPAction.Status(RPAction.Status.FAIL);
+  }
+  
+}
+
