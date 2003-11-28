83fefae4eb5a19d2372c1969ae82045fa3b1993c
miguel <arianne_rpg@users.sourceforge.net>
2003-11-28 22:24:51 +0000

    Update traces, fixed style.

--- game/MarauroaRPRuleProcessor.java
+++ game/MarauroaRPRuleProcessor.java
@@ -6,27 +6,25 @@
 
 package marauroa.game;
 
-import java.util.LinkedList;
+import java.util.List;
 import marauroa.marauroad;
 
-public class MarauroaRPRuleProcessor
-  implements RPRuleProcessor
-{
+public class MarauroaRPRuleProcessor implements RPRuleProcessor
+  {
   private RPZone zone;
   
   public MarauroaRPRuleProcessor(RPZone zone)
-  {
+    {
     this.zone=zone;
-  }
+    }
   
-  public RPAction.Status execute(RPObject.ID id, LinkedList list)
-  {
+  public RPAction.Status execute(RPObject.ID id, List list)
+    {
     marauroad.trace("RPRuleProcessor::execute",">");
     /* TODO: Implement action procession code */
     marauroad.trace("RPRuleProcessor::execute","<");
     
     return new RPAction.Status(RPAction.Status.FAIL);
+    }
   }
-  
-}
 
