029c78553bcaa5349ef2b3c408506984f2a8ca1a
root <root777@users.sourceforge.net>
2003-11-29 18:45:54 +0000

    RPActionList

--- game/MarauroaRPRuleProcessor.java
+++ game/MarauroaRPRuleProcessor.java
@@ -10,21 +10,21 @@ import java.util.List;
 import marauroa.marauroad;
 
 public class MarauroaRPRuleProcessor implements RPRuleProcessor
-  {
+{
   private RPZone zone;
   
   public MarauroaRPRuleProcessor(RPZone zone)
-    {
+  {
     this.zone=zone;
-    }
+  }
   
-  public RPAction.Status execute(RPObject.ID id, List list)
-    {
+  public RPAction.Status execute(RPObject.ID id, RPActionList list)
+  {
     marauroad.trace("RPRuleProcessor::execute",">");
     /* TODO: Implement action procession code */
     marauroad.trace("RPRuleProcessor::execute","<");
     
     return new RPAction.Status(RPAction.Status.FAIL);
-    }
   }
+}
 
