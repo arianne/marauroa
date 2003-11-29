9dd10659f192410b0cfd7605784138f3426a8bde
miguel <arianne_rpg@users.sourceforge.net>
2003-11-29 19:12:47 +0000

    Moved files.

---
+++ src/marauroa/RunTests.java
@@ -0,0 +1,40 @@
+package marauroa.test;
+import junit.framework.*;
+
+/**
+ * TestSuite that runs all the sample tests
+ */
+public class RunTests
+  {
+  public static void main (String[] args) 
+    {
+ 	//junit.swingui.TestRunner.run(RunTests.class);
+ 	junit.textui.TestRunner.run(suite());
+	}
+  
+  public static Test suite ( ) 
+    {
+	TestSuite suite= new TestSuite("All marauroa Tests");
+	
+	suite.addTest(new TestSuite(marauroa.net.Test_SerializerByte.class));
+	suite.addTest(new TestSuite(marauroa.net.Test_SerializerShort.class));
+	suite.addTest(new TestSuite(marauroa.net.Test_SerializerInt.class));
+	suite.addTest(new TestSuite(marauroa.net.Test_SerializerByteArray.class));	
+	suite.addTest(new TestSuite(marauroa.net.Test_SerializerString.class));	
+	
+	suite.addTest(new TestSuite(marauroa.net.Test_Messages.class));
+	suite.addTest(new TestSuite(marauroa.net.Test_MessageFactory.class));
+	
+	suite.addTest(new TestSuite(marauroa.net.Test_NetworkServerManager.class));
+	
+	suite.addTest(new TestSuite(marauroa.game.Test_PlayerDatabase.class));
+	suite.addTest(new TestSuite(marauroa.game.Test_PlayerEntryContainer.class));
+	suite.addTest(new TestSuite(marauroa.game.Test_Attributes.class));	
+	suite.addTest(new TestSuite(marauroa.game.Test_RPZone.class));
+	suite.addTest(new TestSuite(marauroa.game.Test_RPObject.class));
+	
+	suite.addTest(new TestSuite(marauroa.Test_Configuration.class));
+
+	return suite;
+	}
+  }
