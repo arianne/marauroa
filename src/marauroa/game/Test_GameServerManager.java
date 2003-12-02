d2a3a5cfd7aeb72d26a31f801086216b7c78a3d6
miguel <arianne_rpg@users.sourceforge.net>
2003-12-02 00:12:58 +0000

    no message

---
+++ src/marauroa/game/Test_GameServerManager.java
@@ -0,0 +1,149 @@
+package marauroa.game;
+
+import junit.framework.*;
+import marauroa.game.*;
+import marauroa.net.*;
+import java.net.*;
+
+public class Test_GameServerManager extends TestCase
+  {
+  public static Test suite ( ) 
+    {
+    return new TestSuite(Test_GameServerManager.class);
+	}
+
+  private NetworkClientManager netMan;
+  private NetworkServerManager netServerMan;
+  private GameServerManager gameMan;
+
+  private void createEnviroment()
+    {    
+    try
+      {
+      /* We want to avoid the port in use error */
+      NetConst.marauroa_PORT=NetConst.marauroa_PORT+1;
+      
+      netMan=new NetworkClientManager("127.0.0.1");
+      netServerMan=new NetworkServerManager();
+      gameMan= new GameServerManager(netServerMan);
+      }
+    catch(java.net.SocketException e)
+      {
+      fail(e.getMessage());
+      }
+    
+    assertTrue("NetworkManager and GameManager inited",netMan!=null && gameMan!=null);
+    
+    try
+      {
+      PlayerDatabase playerDatabase=PlayerDatabaseFactory.getDatabase("MemoryPlayerDatabase");
+      assertFalse(playerDatabase.hasPlayer("Test Player"));
+
+      playerDatabase.addPlayer("Test Player","Test Password");
+
+      RPObject SonGoku=new RPObject();
+      SonGoku.put("object_id","1");
+      SonGoku.put("name","Son Goku");
+      playerDatabase.addCharacter("Test Player", "Son Goku",SonGoku);
+      }
+    catch(Exception e)
+      {
+      fail(e.getMessage());
+      }
+    }
+    
+  public void testMainProcedures()
+    {
+	createEnviroment();
+	
+    try
+      {
+      InetSocketAddress address=new InetSocketAddress("127.0.0.1",NetConst.marauroa_PORT);
+      netMan.addMessage(new MessageC2SLogin(address,"Test Player","Test Password"));
+      int clientid=-1;
+      
+      int recieved=0;
+      while(recieved!=2)
+        {
+        Message msg=null;
+        while(msg==null) msg=netMan.getMessage();
+        
+        if(msg instanceof MessageS2CLoginACK)
+          {
+          assertTrue("Correct login parameters",true);
+          clientid=msg.getClientID();
+          ++recieved;
+          }
+        else if(msg instanceof MessageS2CCharacterList)
+          {
+          assertTrue("Recieved character list",true);
+          ++recieved;
+          }
+        else
+          {
+          fail("ERROR: Can't login. Got "+msg.toString());
+          }        
+        }
+
+      Message msgCC=new MessageC2SChooseCharacter(address,"Son Goku");
+      msgCC.setClientID(clientid);      
+      netMan.addMessage(msgCC);
+      
+      while(recieved!=3)
+        {
+        Message msg=null;
+        while(msg==null) msg=netMan.getMessage();
+
+        if(msg instanceof MessageS2CChooseCharacterACK)
+          {
+          assertTrue("Correct character parameters",true);
+          ++recieved;
+          }
+        else
+          {
+          fail("ERROR: Can't choose character. Got "+msg.toString());
+          }        
+        }
+        
+      Message msgL=new MessageC2SLogout(address);
+      msgL.setClientID(clientid);      
+      netMan.addMessage(msgL);
+      
+      while(recieved!=4)
+        {
+        Message msg=null;
+        while(msg==null) msg=netMan.getMessage();
+
+        if(msg instanceof MessageS2CLogoutACK)
+          {
+          assertTrue("Correct logout parameters",true);
+          ++recieved;
+          }
+        else
+          {
+          fail("ERROR: Can't logout. Got "+msg.toString());
+          }        
+        }
+      }
+    catch(Exception e)
+      {
+      }
+    finally
+      {
+      finalizeEnviroment();
+      }
+    }  
+
+  private void finalizeEnviroment()
+    {
+    gameMan.finish();
+    
+    /* To break the wait on a message */
+    InetSocketAddress address=new InetSocketAddress("127.0.0.1",NetConst.marauroa_PORT);
+    netMan.addMessage(new MessageC2SLogout(address));
+    
+    netServerMan.finish();
+    
+    assertTrue("Shutdown correctly",true);
+    }    
+  }
