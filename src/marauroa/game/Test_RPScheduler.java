81f625a5e438d6a38bd6f84bfd0b759389f66c2d
miguel <arianne_rpg@users.sourceforge.net>
2003-12-03 17:31:24 +0000

    no message

---
+++ src/marauroa/game/Test_RPScheduler.java
@@ -0,0 +1,18 @@
+package marauroa.game;
+
+import junit.framework.*;
+import marauroa.net.*;
+import marauroa.*;
+import java.io.*;
+
+public class Test_RPScheduler extends TestCase
+  {
+  public static Test suite ( ) 
+    {
+    return new TestSuite(Test_RPScheduler.class);
+	}
+	
+  public void testRPScheduler()
+    {
+    }
+  }
