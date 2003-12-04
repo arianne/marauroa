584a22106e377283580372ceb7fb5af32fbe6441
root <root777@users.sourceforge.net>
2003-12-04 22:49:11 +0000

    new class

---
+++ src/simplegame/actions/GetCharacterListAction.java
@@ -0,0 +1,20 @@
+/**
+ * GetCharacterListAction.java
+ *
+ * @author Waldemar Tribus
+ */
+
+package simplegame.actions;
+
+import marauroa.game.RPAction;
+
+public class GetCharacterListAction
+  extends RPAction
+{
+  public final static int ACTION_GETCHARLIST=3;
+    public GetCharacterListAction()
+  {
+    put("type",ACTION_GETCHARLIST);
+  }
+}
+
