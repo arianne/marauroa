eb5869e313cbb416d1ccac0ac12befcf23d5822d
miguel <arianne_rpg@users.sourceforge.net>
2003-12-08 14:34:38 +0000

    1001 :-o

---
+++ src/the1001/the1001RPZone.java
@@ -0,0 +1,23 @@
+/* $Id: the1001RPZone.java,v 1.1 2003/12/08 14:34:38 arianne_rpg Exp $ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package the1001;
+
+import marauroa.game.MarauroaRPZone;
+
+public class the1001RPZone extends MarauroaRPZone
+  {
+  public the1001RPZone()
+    {
+    }
+  }
+  
