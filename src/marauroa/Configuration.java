fedf7b5f145ad93728921fb9ecd34b07f6c67d07
miguel <arianne_rpg@users.sourceforge.net>
2003-12-08 01:06:29 +0000

    Added Header to every file

--- src/marauroa/Configuration.java
+++ src/marauroa/Configuration.java
@@ -1,3 +1,15 @@
+/* $Id: Configuration.java,v 1.5 2003/12/08 01:06:29 arianne_rpg Exp $ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package marauroa;
 
 import marauroa.*;
