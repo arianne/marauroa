3a3dd912fe3c4e1f90129aeb085f0f9e88daefe8
miguel <arianne_rpg@users.sourceforge.net>
2003-12-12 15:57:11 +0000

    started the basic work on Gladiators

--- src/the1001/the1001RPRuleProcessor.java
+++ src/the1001/the1001RPRuleProcessor.java
@@ -1,4 +1,4 @@
-/* $Id: RPRuleProcessor.java,v 1.9 2003/12/12 15:57:11 arianne_rpg Exp $ */
+/* $Id: the1001RPRuleProcessor.java,v 1.1 2003/12/12 15:57:11 arianne_rpg Exp $ */
 /***************************************************************************
  *                      (C) Copyright 2003 - Marauroa                      *
  ***************************************************************************
@@ -10,25 +10,45 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-package marauroa.game;
+package the1001;
 
-/** Interface for the class that is in charge of executing actions.
- *  Implement it to personalize the game */
-public interface RPRuleProcessor
+import marauroa.game.*;
+
+public class the1001RPRuleProcessor implements RPRuleProcessor
   {
+  RPZone zone;
+  
+  public the1001RPRuleProcessor()
+    {
+    }
+
   /** Set the context where the actions are executed.
    *  @param zone The zone where actions happens. */
-  public void setContext(RPZone zone);
+  public void setContext(RPZone zone)
+    {
+    }
+    
   /** Pass the whole list of actions so that it can approve or deny the actions in it.
    *  @param id the id of the object owner of the actions.
    *  @param actionList the list of actions that the player wants to execute. */
-  public void approvedActions(RPObject.ID id, RPActionList actionList);
+  public void approvedActions(RPObject.ID id, RPActionList actionList)
+    {
+    }
+    
   /** Execute an action in the name of a player.
    *  @param id the id of the object owner of the actions.
    *  @param action the action to execute
    *  @returns the action status, that can be Success, Fail or incomplete, please 
    *      refer to Actions Explained for more info. */
-  public RPAction.Status execute(RPObject.ID id, RPAction action);
+  public RPAction.Status execute(RPObject.ID id, RPAction action)
+    {
+    return RPAction.STATUS_FAIL;
+    }
+    
   /** Notify it when a new turn happens */
-  public void nextTurn();
+  public void nextTurn()
+    {
+    }
   }
+    
+  
