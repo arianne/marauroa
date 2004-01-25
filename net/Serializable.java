0d717378c0470d3c6b6211c912da28500e47507e
miguel <arianne_rpg@users.sourceforge.net>
2004-01-25 22:56:32 +0000

    no message

--- net/Serializable.java
+++
@@ -1,8 +0,0 @@
-package marauroa.net;
-
-public interface Serializable
-  {
-  void writeObject(marauroa.net.OutputSerializer out) throws java.io.IOException;
-  void readObject(marauroa.net.InputSerializer in) throws java.io.IOException,
-    java.lang.ClassNotFoundException;  	  
-  };
