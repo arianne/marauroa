57c3191066a52847e9a9c88203ab1cc213dfac14
root <root777@users.sourceforge.net>
2004-02-07 13:02:54 +0000

    memory usage added

--- src/marauroa/JStatistics.java
+++ src/marauroa/JStatistics.java
@@ -26,121 +26,134 @@ public class JStatistics
   private JLabel lblActions;
   private JLabel lblObjects;
   private JLabel lblPlayers;
-  private Timer timer;
+	private JLabel lblMemory;
+  private Timer  timer;
   
   public JStatistics()
   {
-    super(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    gbc.gridwidth  = 1;
-    gbc.gridheight = 1;
-    gbc.anchor     = GridBagConstraints.WEST;
-    gbc.fill       = GridBagConstraints.NONE;
-    gbc.weightx    = 0.1;
-    gbc.weighty    = 0.1;
-    gbc.insets = new Insets(2,2,2,10);
-    
-    JLabel lbl = new JLabel("Bytes Received/Sent");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblBytes = new JLabel("Unknown");
-    add(lblBytes,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    lbl = new JLabel("Packets Received/Sent");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblPackets = new JLabel("Unknown");
-    add(lblPackets,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    lbl = new JLabel("Messages Received/Sent/Incorrect");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblMessages = new JLabel("Unknown");
-    add(lblMessages,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    lbl = new JLabel("Actions Added/Invalid");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblActions = new JLabel("Unknown");
-    add(lblActions,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    lbl = new JLabel("Objects Added/Removed/Now");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblObjects = new JLabel("Unknown");
-    add(lblObjects,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    lbl = new JLabel("Players Login/Logout/Online/Invalid Login");
-    add(lbl,gbc);
-    gbc.gridx++;
-    lblPlayers = new JLabel("Unknown");
-    add(lblPlayers,gbc);
-    gbc.gridx = 0;
-    gbc.gridy++;
-    
-    timer = new Timer(1000,new ActionListener()
-                      {
-          public void actionPerformed(ActionEvent e)
-          {
-            Statistics.addBytesRecv(1);
-            updateStats(Statistics.getVariables());
-          }
-        });
-    updateStats(Statistics.getVariables());
-    timer.start();
+		super(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth  = 1;
+		gbc.gridheight = 1;
+		gbc.anchor     = GridBagConstraints.WEST;
+		gbc.fill       = GridBagConstraints.NONE;
+		gbc.weightx    = 0.1;
+		gbc.weighty    = 0.1;
+		gbc.insets = new Insets(2,2,2,10);
+		
+		JLabel lbl = new JLabel("Bytes Received/Sent");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblBytes = new JLabel("Unknown");
+		add(lblBytes,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Packets Received/Sent");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblPackets = new JLabel("Unknown");
+		add(lblPackets,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Messages Received/Sent/Incorrect");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblMessages = new JLabel("Unknown");
+		add(lblMessages,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Actions Added/Invalid");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblActions = new JLabel("Unknown");
+		add(lblActions,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Objects Added/Removed/Now");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblObjects = new JLabel("Unknown");
+		add(lblObjects,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Players Login/Logout/Online/Invalid Login");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblPlayers = new JLabel("Unknown");
+		add(lblPlayers,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		lbl = new JLabel("Memory Allocated/Used/Free");
+		add(lbl,gbc);
+		gbc.gridx++;
+		lblMemory = new JLabel("Unknown");
+		add(lblMemory,gbc);
+		gbc.gridx = 0;
+		gbc.gridy++;
+		
+		timer = new Timer(1000,new ActionListener()
+											{
+					public void actionPerformed(ActionEvent e)
+					{
+						Statistics.addBytesRecv(1);
+						updateStats(Statistics.getVariables());
+					}
+				});
+		updateStats(Statistics.getVariables());
+		timer.start();
   }
   
   public void updateStats(Statistics.GatheredVariables vars)
   {
-    lblBytes.setText(vars.bytesRecv + "/"+ vars.bytesSend);
-    lblPackets.setText("Not there yet");
-    lblMessages.setText(vars.messagesRecv + "/" + vars.messagesSend + "/" + vars.messagesIncorrect);
-    lblActions.setText(vars.actionsAdded + "/" + vars.actionsInvalid);
-    lblObjects.setText(vars.objectsAdded + "/" + vars.objectsRemoved + "/" + vars.objectsNow);
-    lblPlayers.setText(vars.playersLogin + "/" + vars.playersLogout + "/" + vars.playersOnline + "/" +vars.playersInvalidLogin);
+		lblBytes.setText(vars.bytesRecv + "/"+ vars.bytesSend);
+		lblPackets.setText("Not there yet");
+		lblMessages.setText(vars.messagesRecv + "/" + vars.messagesSend + "/" + vars.messagesIncorrect);
+		lblActions.setText(vars.actionsAdded + "/" + vars.actionsInvalid);
+		lblObjects.setText(vars.objectsAdded + "/" + vars.objectsRemoved + "/" + vars.objectsNow);
+		lblPlayers.setText(vars.playersLogin + "/" + vars.playersLogout + "/" + vars.playersOnline + "/" +vars.playersInvalidLogin);
+		long total = Runtime.getRuntime().totalMemory();
+		long free  = Runtime.getRuntime().freeMemory();
+		long used  = total-free;
+		lblMemory.setText(total+"/"+used+"/"+free);
   }
   
   public void setRefreshDelay(int delay)
   {
-    timer.stop();
-    timer.setDelay(delay);
-    timer.setInitialDelay(delay);
-    timer.start();
+		timer.stop();
+		timer.setDelay(delay);
+		timer.setInitialDelay(delay);
+		timer.start();
   }
   
   public static JStatistics showStatistics()
   {
-    JFrame frm = new JFrame("marauroad statistics");
-    frm.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-    frm.setResizable(false);
-    JStatistics js = new JStatistics();
-    frm.setContentPane(js);
-    frm.pack();
-    frm.show();
-    return(js);
+		JFrame frm = new JFrame("marauroad statistics");
+		frm.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frm.setResizable(false);
+		JStatistics js = new JStatistics();
+		frm.setContentPane(js);
+		frm.pack();
+		frm.show();
+		return(js);
   }
   
   public static void main(String argv[])
   {
-    JStatistics js = showStatistics();
-    try
-    {
-      Thread.sleep(5000);
-    }
-    catch (InterruptedException e) {}
-    js.setRefreshDelay(2000);
+		JStatistics js = showStatistics();
+		try
+		{
+			Thread.sleep(5000);
+		}
+		catch (InterruptedException e) {}
+		js.setRefreshDelay(2000);
   }
   
 }
