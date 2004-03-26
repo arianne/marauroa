b90f4a832b76a169529320305ae021b5077382ae
miguel <arianne_rpg@users.sourceforge.net>
2004-03-26 13:27:19 +0000

    no message

--- src/marauroa/marauroad.java
+++ src/marauroa/marauroad.java
@@ -1,4 +1,4 @@
-/* $Id: marauroad.java,v 1.55 2004/03/25 22:20:43 arianne_rpg Exp $ */
+/* $Id: marauroad.java,v 1.56 2004/03/26 13:27:19 arianne_rpg Exp $ */
 /***************************************************************************
  *                      (C) Copyright 2003 - Marauroa                      *
  ***************************************************************************
@@ -223,12 +223,12 @@ public class marauroad extends Thread
     }
 
 // Debug setting
-  private static String[] allowed={"*"};
-  private static String[] rejected={};
+//  private static String[] allowed={"*"};
+//  private static String[] rejected={};
 
 // Production setting
-//  private static String[] allowed={"RPCode","the1001"};
-//  private static String[] rejected={"the1001RPRuleProcessor::nextTurn","the1001RPRuleProcessor::removeOneTurnAttributes","the1001RPRuleProcessor::execute"};
+  private static String[] allowed={"RPCode","the1001"};
+  private static String[] rejected={"the1001RPRuleProcessor::nextTurn","the1001RPRuleProcessor::removeOneTurnAttributes","the1001RPRuleProcessor::execute"};
 
   public static void trace(String module,String event)
     {
