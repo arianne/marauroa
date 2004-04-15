6db36564f554a150b316c484d712094d533266e1
miguel <arianne_rpg@users.sourceforge.net>
2004-04-15 12:22:25 +0000

    no message

--- src/marauroa/nullClient.java
+++ src/marauroa/nullClient.java
@@ -102,12 +102,13 @@ class TestClient
               System.err.println("We are out of sync. Waiting for sync perception");
               System.err.println("Expected"+previous_timestamp+" but we got "+msgPer.getTimestamp());
               outofsync=true;
+              /* TODO: Try to regain sync by getting more messages in the hope of getting the out of order perception */                
               }
             else
               {
               previous_timestamp=msgPer.getTimestamp();
               System.err.println("Got Perception - "+msgPer.getTypePerception()+" - "+msgPer.getTimestamp());
-              System.out.println(msgPer.getTypePerception());
+              System.out.println(msgPer.getTypePerception()+" - "+msgPer.getTimestamp());
           
               Iterator it;
               it=msgPer.getDeletedRPObjects().iterator();
