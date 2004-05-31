39c7162f07974c9be504b2fd5d73fedffda99110
root <root777@users.sourceforge.net>
2004-05-31 07:20:46 +0000

    CVS ID maintained

--- src/marauroa/game/python/PythonAI.java
+++ src/marauroa/game/python/PythonAI.java
@@ -1,4 +1,18 @@
+/* $Id: PythonAI.java,v 1.3 2004/05/31 07:20:46 root777 Exp $ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 package marauroa.game.python;
+
 import  marauroa.game.*;
 
 public class PythonAI 
--- src/marauroa/game/python/PythonRP.java
+++ src/marauroa/game/python/PythonRP.java
@@ -1,4 +1,17 @@
 
+/* $Id: PythonRP.java,v 1.4 2004/05/31 07:20:46 root777 Exp $ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 package marauroa.game.python;
 
 import marauroa.game.*;
