678a1fa3e4f699fec4754235000674e164a24040
miguel <arianne_rpg@users.sourceforge.net>
2005-01-23 20:33:59 +0000

    no message

---
+++ extras/template_game/template_game.py
@@ -0,0 +1,38 @@
+from marauroa.game.python import *
+from marauroa.game import *
+from marauroa.net import *
+from marauroa import *
+
+import java.util.*
+
+class TemplateRP(PythonRP):
+  def __init__(self, zone):
+    self._zone=zone
+
+  def execute(self, id, action):
+    return 0
+
+  def nextTurn(self):
+      pass
+
+  def onInit(self, object):
+      return 0
+
+  def onExit(self, objectid):
+      return 0
+
+  def onTimeout(self, objectid):
+      return 0
+
+class TemplateZone(PythonZone):
+    def __init__(self, zone):
+        self._zone=zone
+
+    def onInit(self):
+        pass
+
+    def onFinish(self):
+        pass
+
+    def serializeMap(self, objectid):
+        return java.util.LinkedList()
