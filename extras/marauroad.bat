99fa2f3fc811b18a9615c9c6d7eab3a5bbdea78d
miguel <arianne_rpg@users.sourceforge.net>
2005-04-05 18:51:33 +0000

    Added attack action

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-0.91.jar;jython.jar;mysql-connector.jar
+set LOCALCLASSPATH=.;marauroa-0.92.jar;jython.jar;mysql-connector.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c marauroa.ini -l
