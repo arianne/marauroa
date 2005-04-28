4ede5aa74817ee1ca917fd8e6a176cca9b7afa42
miguel <arianne_rpg@users.sourceforge.net>
2005-04-28 18:05:56 +0000

    no message

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-0.94.jar;jython.jar;mysql-connector-java-3.1.8-bin.jar
+set LOCALCLASSPATH=.;marauroa-0.94.jar;jython.jar;mysql-connector.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c marauroa.ini -l
