64285c724dcb2a84f1bbf5447cc006e800d557f1
miguel <arianne_rpg@users.sourceforge.net>
2006-03-24 18:07:22 +0000

    Database problem fixed.

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-1.31.jar;jython.jar;mysql-connector.jar;log4j.jar
+set LOCALCLASSPATH=.;marauroa-1.31.1.jar;jython.jar;mysql-connector.jar;log4j.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c marauroa.ini -l
