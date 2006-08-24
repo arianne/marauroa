9902990a29993df391044b52b824729498862b85
hendrik <nhnb@users.sourceforge.net>
2006-08-24 21:32:48 +0000

    accept a normal Map (previously a HashMap was required)

--- src/marauroa/server/net/TCPReader.java
+++ src/marauroa/server/net/TCPReader.java
@@ -15,51 +15,43 @@ import marauroa.server.game.Statistics;
 
 import org.apache.log4j.Logger;
 
-/**
+/** 
  * The active thread in charge of recieving messages from the network.
  */
 class TCPReader extends Thread {
 	private static Logger logger = Logger.getLogger(TCPReader.class);
-
 	private NetworkServerManagerCallback networkServerManager = null;
-
-	private HashMap<InetSocketAddress, Socket> tcpSockets = null;
-
+	
+	private Map<InetSocketAddress, Socket> tcpSockets = null;
 	private Map<Socket, Integer> bytesToRead = new WeakHashMap<Socket, Integer>();
-
 	private Statistics stats = null;
 
 	/**
 	 * Creates a NetworkServerManagerRead
-	 * 
-	 * @param networkServerManager
-	 *            NetworkServerManager
-	 * @param tcpSockets
-	 *            communication end-points
-	 * @param stats
-	 *            Statistics
+	 *
+	 * @param networkServerManager NetworkServerManager 
+	 * @param tcpSockets communication end-points
+	 * @param stats Statistics
 	 */
-	public TCPReader(NetworkServerManagerCallback networkServerManager,
-			HashMap<InetSocketAddress, Socket> tcpSockets, Statistics stats) {
+	public TCPReader(NetworkServerManagerCallback networkServerManager, Map<InetSocketAddress, Socket> tcpSockets, Statistics stats) {
 		super("TCPReader");
 		this.networkServerManager = networkServerManager;
 		this.tcpSockets = tcpSockets;
 		this.stats = stats;
 	}
 
-	/**
+	/** 
 	 * Method that execute the reading. It runs as a active thread forever.
 	 */
 	@Override
 	public void run() {
 		logger.debug("run()");
 		byte[] sizebuffer = new byte[4];
-
+		
 		while (networkServerManager.isStillRunning()) {
 			boolean found = false;
 			long start = System.currentTimeMillis();
-			Map<InetSocketAddress, Socket> temptTcpSockets = (Map<InetSocketAddress, Socket>) tcpSockets
-					.clone();
+			Map<InetSocketAddress, Socket> temptTcpSockets = cloneMap(tcpSockets);
 			for (InetSocketAddress inetSocketAddress : temptTcpSockets.keySet()) {
 				Socket socket = temptTcpSockets.get(inetSocketAddress);
 				if (socket.isClosed()) {
@@ -72,7 +64,7 @@ class TCPReader extends Thread {
 				}
 				try {
 					InputStream is = socket.getInputStream();
-
+					
 					Integer toReadInt = bytesToRead.get(socket);
 					int size = -1;
 					if (toReadInt == null) {
@@ -80,24 +72,19 @@ class TCPReader extends Thread {
 						if (is.available() >= 4) {
 							is.read(sizebuffer);
 							size = (sizebuffer[0] & 0xFF)
-									+ ((sizebuffer[1] & 0xFF) << 8)
-									+ ((sizebuffer[2] & 0xFF) << 16)
-									+ ((sizebuffer[3] & 0xFF) << 24);
+								+ ((sizebuffer[1] & 0xFF) << 8)
+								+ ((sizebuffer[2] & 0xFF) << 16)
+								+ ((sizebuffer[3] & 0xFF) << 24);
 							if (size == 542393671) { // "GET "
-								// This request was not created by the
-								// marauroa-client
-								// but it was created by the HTTP-client to
-								// compare
+								// This request was not created by the marauroa-client
+								// but it was created by the HTTP-client to compare
 								// version numbers. ==> Close it.
 								OutputStream os = socket.getOutputStream();
-								os.write("500 This is not a webserver\r\n\r\n"
-										.getBytes());
+								os.write("500 This is not a webserver\r\n\r\n".getBytes());
 								os.flush();
 								os.close();
-								logger
-										.warn("Closing connection because packet-size is magic-number \"GET \".");
-								networkServerManager
-										.disconnectClient(inetSocketAddress);
+								logger.warn("Closing connection because packet-size is magic-number \"GET \".");
+								networkServerManager.disconnectClient(inetSocketAddress);
 								continue;
 							}
 							bytesToRead.put(socket, new Integer(size));
@@ -112,29 +99,27 @@ class TCPReader extends Thread {
 						byte[] buffer = new byte[size];
 						is.read(buffer);
 						logger.debug("Received TCP Packet");
-
-						/** * Statistics ** */
+		
+						/*** Statistics ***/
 						stats.add("Bytes recv", size);
 						stats.add("Message recv", 1);
-
-						networkServerManager.receiveMessage(buffer,
-								inetSocketAddress);
+		
+						networkServerManager.receiveMessage(buffer, inetSocketAddress);
 						bytesToRead.remove(socket);
 					}
 				} catch (SocketTimeoutException e) {
 					logger.warn(e + " (" + socket.getInetAddress() + ")", e);
 					networkServerManager.disconnectClient(inetSocketAddress);
-				} catch (SocketException e) {
-					logger.warn(e + " (" + socket.getInetAddress() + ")", e);
-					networkServerManager.disconnectClient(inetSocketAddress);
+                } catch (SocketException e) {
+                    logger.warn(e + " (" + socket.getInetAddress() + ")", e);
+                    networkServerManager.disconnectClient(inetSocketAddress);
 				} catch (IOException e) {
 					logger.warn(e + " (" + socket.getInetAddress() + ")", e);
 					networkServerManager.disconnectClient(inetSocketAddress);
 				} catch (Exception e) {
 					/* Report the exception */
-					logger.error("error while processing tcp-packets ("
-							+ socket.getInetAddress() + ")", e);
-					networkServerManager.disconnectClient(inetSocketAddress);
+					logger.error("error while processing tcp-packets (" + socket.getInetAddress() + ")", e);
+                    networkServerManager.disconnectClient(inetSocketAddress);
 				}
 			}
 
@@ -146,9 +131,7 @@ class TCPReader extends Thread {
 						Thread.sleep(wait);
 					} else {
 						wait = wait * -1;
-						logger
-								.warn("Turn duration overflow by " + wait
-										+ " ms");
+						logger.warn("Turn duration overflow by " + wait + " ms");
 					}
 				} catch (InterruptedException e) {
 					logger.error(e, e);
@@ -158,5 +141,22 @@ class TCPReader extends Thread {
 
 		networkServerManager.finishedReadThread();
 		logger.debug("run() finished");
+	}
+
+	/**
+	 * Clones a map. We cannot use .clone() because synchronized maps
+	 * do not implement Cloneable :-/
+	 *
+	 * @param map map to clone
+	 * @return cloned map
+	 */
+	private Map<InetSocketAddress, Socket> cloneMap(Map<InetSocketAddress, Socket> map) {
+		Map<InetSocketAddress, Socket> res = new HashMap<InetSocketAddress, Socket>();
+		synchronized (map) {
+			for (InetSocketAddress key : map.keySet()) {
+				res.put(key, map.get(key));
+			}
+		}
+		return res;
 	}
 }
