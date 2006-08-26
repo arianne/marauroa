3a4c1decb270dbbc44bef91605682acac5bdb504
hendrik <nhnb@users.sourceforge.net>
2006-08-26 21:05:28 +0000

    moved client.io from Stendhal to Marauroa

---
+++ src/marauroa/client/io/Persistence.java
@@ -0,0 +1,69 @@
+package marauroa.client.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.AccessControlException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Allows transparent access to files. Subclasses implement Persistence
+ * for normal and webstart environment.
+ *
+ * @author hendrik
+ */
+public abstract class Persistence {
+	private static Logger logger = Logger.getLogger(Persistence.class);
+	private static Persistence instance = null;
+	private static boolean WEB_START_SANDBOX = false;
+
+	/**
+	 * Returns the Persistence manager for this environmen
+	 *
+	 * @return Persistence
+	 */
+	public static Persistence get() {
+		if (instance == null) {
+			try {
+				System.getProperty("user.home");
+			} catch (AccessControlException e) {
+				WEB_START_SANDBOX = true;
+			}
+
+			if (WEB_START_SANDBOX) {
+				try {
+					// we use reflection to prevent any runtime dependency on jnlp.jar
+					// outside webstart. So we do not have to distribute jnlp.jar
+					Class clazz = Class.forName("games.stendhal.client.io.WebstartPersistence");
+					instance = (Persistence) clazz.newInstance();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+					logger.error(e, e);
+				}
+			} else {
+				instance = new FileSystemPersistence();
+			}
+		}
+		return instance;
+	}
+	
+	/**
+	 * Gets an input stream to this "virtual" file
+	 *
+	 * @param filename filename (without path)
+	 * @return InputStream
+	 * @throws IOException on io error
+	 */
+	public abstract InputStream getInputStream(String filename) throws IOException;
+
+	/**
+	 * Gets an output stream to this "virtual" file
+	 *
+	 * @param filename filename (without path)
+	 * @return InputStream
+	 * @throws IOException on io error
+	 */
+	public abstract OutputStream getOutputStream(String filename) throws IOException;
+
+}
