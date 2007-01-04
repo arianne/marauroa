d142baa0a5f0bce4500eecf484e184789a36f734
hendrik <nhnb@users.sourceforge.net>
2007-01-04 13:27:08 +0000

    fixed exception on second login with different case

--- src/marauroa/server/game/PlayerEntryContainer.java
+++ src/marauroa/server/game/PlayerEntryContainer.java
@@ -1,4 +1,4 @@
-/* $Id: PlayerEntryContainer.java,v 1.15 2006/12/10 19:35:11 nhnb Exp $ */
+/* $Id: PlayerEntryContainer.java,v 1.15.2.1 2007/01/04 13:27:08 nhnb Exp $ */
 /***************************************************************************
  *                      (C) Copyright 2003 - Marauroa                      *
  ***************************************************************************
@@ -533,7 +533,7 @@ public class PlayerEntryContainer {
 						.getValue();
 
 				if (playerEntry.username != null
-						&& playerEntry.username.equals(username)) {
+						&& playerEntry.username.equalsIgnoreCase(username)) {
 					return playerEntry.clientid;
 				}
 			}
