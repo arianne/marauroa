d9ce7c18c93204760a666392b8c5da9330432f6c
hendrik <nhnb@users.sourceforge.net>
2007-01-06 05:24:19 +0000

    improve XML escaping

--- src/marauroa/server/game/Statistics.java
+++ src/marauroa/server/game/Statistics.java
@@ -1,4 +1,4 @@
-/* $Id: Statistics.java,v 1.14 2006/12/02 15:53:30 nhnb Exp $ */
+/* $Id: Statistics.java,v 1.15 2007/01/06 05:24:19 nhnb Exp $ */
 /***************************************************************************
  *                      (C) Copyright 2003 - Marauroa                      *
  ***************************************************************************
@@ -193,6 +193,6 @@ public class Statistics implements StatisticsMBean {
 	 * @return escaped strings
 	 */
 	private static String escapeML(String param) {
-		return param.replace("&", "&amp;").replace("\"", "&quot;");
+		return param.replace("&", "&amp;").replace("\"", "&quot;").replace("<", "&lt;").replace(">", "&gt;");
 	}
 }
