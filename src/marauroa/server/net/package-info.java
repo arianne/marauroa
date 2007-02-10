f720854b663d2e55d13e319723c4857e71f846f9
miguel <arianne_rpg@users.sourceforge.net>
2007-02-10 17:02:43 +0000

    Documented a bit more :)

---
+++ src/marauroa/server/net/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package stores the network related code.
+ * <p>
+ * It defines the interfaces for implementing a network manager ({@link INetworkServerManager}) 
+ * and the interface for implement a disconnection listener ({@link IDisconnectedListener})
+ */
+package marauroa.server.net;
