07b6482eb51776665f8392bceaa4eadef2704485
miguel <arianne_rpg@users.sourceforge.net>
2007-03-01 21:53:42 +0000

    Fixed ant file. Still missing javadocs and libraries at source file so anyone can easily build Marauroa.

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-1.32.jar;jython.jar;mysql-connector.jar;log4j.jar
+set LOCALCLASSPATH=.;marauroa-2.0.jar;libs/jython.jar;libs/mysql-connector.jar;libs/log4j.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c marauroa.ini -l
