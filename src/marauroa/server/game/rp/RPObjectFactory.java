4ce07874b6a93a5d001468402389be58de40fbed
miguel <arianne_rpg@users.sourceforge.net>
2007-05-25 10:14:05 +0000

    Copied Database creation code for initialize the factory.

--- src/marauroa/server/game/rp/RPObjectFactory.java
+++ src/marauroa/server/game/rp/RPObjectFactory.java
@@ -1,6 +1,12 @@
 package marauroa.server.game.rp;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import marauroa.common.Configuration;
+import marauroa.common.Log4J;
 import marauroa.common.game.RPObject;
+import marauroa.server.game.db.DatabaseFactory;
 
 
 /**
@@ -8,10 +14,69 @@ import marauroa.common.game.RPObject;
  * This class is invoked by whatever that load an object into the server:
  * - JDBCDatabase.
  * 
+ * To make it work on your game you need to subclass and implement:
+ * 
+ *   static RPObjectFactory getFactory()
+ * 
+ * The method will be called through refletions.
  * @author miguel
  *
  */
 public class RPObjectFactory {
+	/** the logger instance. */
+	private static final marauroa.common.Logger logger = Log4J.getLogger(DatabaseFactory.class);
+
+	/**
+	 * This method returns an instance of RPObjectFactory choosen using the
+	 * Configuration file.
+	 *
+	 * @return A shared instance of RPObjectFactory
+	 * @throws xception
+	 */
+	public static RPObjectFactory get() throws NoFactoryConfException {
+		try {
+	        Configuration conf = Configuration.getConfiguration();
+	        String factoryName = conf.get("factory_implementation");
+
+	        return get(factoryName);
+        } catch (Exception e) {
+        	logger.fatal("Can't create factory", e);
+        	throw new NoFactoryConfException(e);
+        }
+	}
+
+	/**
+	 * This method returns an instance of RPObjectFactory choosen using the
+	 * param.
+	 *
+	 * @param factoryName
+	 *            A String containing the type of factory. It should be the
+	 *            complete class name. ie: marauroa.server.game.rp.RPObjectFactory
+	 *
+	 * @return A shared instance of RPObjectFactory
+	 * @throws Exception 
+	 */
+	public static RPObjectFactory get(String factoryName) throws NoFactoryConfException {
+		try {
+	        Class databaseClass = Class.forName(factoryName);
+	        java.lang.reflect.Method singleton = databaseClass.getDeclaredMethod("getFactory");
+	        return (RPObjectFactory) singleton.invoke(null);
+        } catch (Exception e) {
+        	logger.fatal("Can't create factory", e);
+        	throw new NoFactoryConfException(e);
+        }
+	}
+	
+	private static RPObjectFactory singleton;
+	
+	static RPObjectFactory getFactory() {
+		if(singleton==null) {
+			singleton=new RPObjectFactory();
+		}
+		
+		return singleton;
+	}
+
 	/**
 	 * This method is called when object is serialized back from database to
 	 * zone, so you can define which subclass of RPObject we are going to use.
