739bc6f597ff6bda183e355eba61071ea6ff6af7
hendrik <nhnb@users.sourceforge.net>
2007-05-26 12:39:12 +0000

    added id column and create index for gameEvents

--- src/marauroa/server/marauroa_init.sql
+++ src/marauroa/server/marauroa_init.sql
@@ -66,13 +66,21 @@ create table if not exists statistics
 
 create table if not exists gameEvents
   (
+  id integer auto_increment not null,
   timedate timestamp,
-  
   source varchar(64),
   event  varchar(64),
   param1 varchar(128),
-  param2 varchar(1024)
+  param2 varchar(255),
+  PRIMARY KEY(id)
   );
+  
+create index i_gameEvents_timedate ON gameEvents(timedate);
+create index i_gameEvents_source ON gameEvents(source);
+create index i_gameEvents_event  ON gameEvents(event);
+create index i_gameEvents_param1 ON gameEvents(param1);
+create index i_gameEvents_param2 ON gameEvents(param2);
+
   
 create table if not exists banlist
   (
