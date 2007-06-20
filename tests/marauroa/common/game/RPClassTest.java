b75d1e56e755f2da5059f734a5640572621da10a
astridemma <astridemma@users.sourceforge.net>
2007-06-20 21:21:07 +0000

    some extra tests for RPClass equals. mblanch please look at testRPClass()

---
+++ tests/marauroa/common/game/RPClassTest.java
@@ -0,0 +1,39 @@
+package marauroa.common.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RPClassTest {
+
+	@Test
+	public void testEqualsNewObjects() throws Exception {
+		assertEquals("two new Objects should be equal", new RPClass(),
+				new RPClass());
+
+	}
+
+	@Test
+	public void testEqualsNewObjectsName() throws Exception {
+		assertEquals("two new Objects should be equal if Name is Equal",
+				new RPClass(new String("name")),
+				new RPClass(new String("name")));
+		assertFalse("two new Objects should not be equal if Name is not Equal",
+				new RPClass("name").equals(new RPClass("name2")));
+	}
+
+	@Test
+	public void testRPClass() throws Exception {
+		RPClass rp = new RPClass("schnipp");
+		RPClass rp2 = new RPClass("schnipp");
+		RPClass childRp = new RPClass(); //not intializing name causes NPE in subclassOf
+		RPClass childRp2 = new RPClass();
+		childRp.isA(rp);
+		childRp2.isA(rp2);
+		assertEquals(childRp, childRp2);
+		assertTrue(childRp.subclassOf(rp.getName()));
+		assertTrue(childRp.subclassOf(rp2.getName()));
+
+	}
+
+}
