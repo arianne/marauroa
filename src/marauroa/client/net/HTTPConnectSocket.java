de2baffe29dd0e7e36399e3134a0726890116acc
hendrik <nhnb@users.sourceforge.net>
2007-08-14 20:58:25 +0000

    initial checkin

---
+++ src/marauroa/client/net/HTTPConnectSocket.java
@@ -0,0 +1,125 @@
+package marauroa.client.net;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * See http://www.web-cache.com/Writings/Internet-Drafts/draft-luotonen-web-proxy-tunneling-01.txt
+ *
+ * @author hendrik
+ */
+public class HTTPConnectSocket extends Socket {
+	private SocketAddress proxy = null;
+	private static final int TIMEOUT = 60;
+	private static final String HTTP_PREFIX = "HTTP/1.0 xxx";
+	private static final byte[] HTTP_SUFFIX = new byte[] {13, 10, 13, 10}; // CR LF CR LF
+
+
+	/**
+	 * Creates a new HTTPConnectSocket
+	 *
+	 * @param proxy address of http-connect proxy
+	 */
+	public HTTPConnectSocket(SocketAddress proxy) {
+		this.proxy = proxy;
+	}
+
+	@Override
+	public void connect(SocketAddress endpoint, int timeout) throws IOException {
+		super.connect(proxy, timeout);
+		setupHttpConnect(endpoint);
+	}
+
+	@Override
+	public void connect(SocketAddress endpoint) throws IOException {
+		super.connect(proxy);
+		setupHttpConnect(endpoint);
+	}
+
+
+	private void setupHttpConnect(SocketAddress endpoint) throws IOException {
+		sendConnect(endpoint);
+		verifyConnection();
+	}
+
+	private void sendConnect(SocketAddress endpoint) throws IOException {
+		if (!(endpoint instanceof InetSocketAddress)) {
+			throw new IOException("Unkown endpoint object. Exspected InetSocketAddress but got " + endpoint.getClass());
+		}
+		InetSocketAddress server = (InetSocketAddress) endpoint;
+
+		/*
+	 	CONNECT home.netscape.com:443 HTTP/1.0
+        User-agent: Mozilla/4.0
+        Proxy-authorization: basic dGVzdDp0ZXN0
+		*/
+
+		OutputStream os = super.getOutputStream();
+		StringBuffer connect = new StringBuffer("CONNECT ");
+		connect.append(server.getHostName());
+		connect.append(":");
+		connect.append(server.getPort());
+		connect.append(" HTTP/1.1\r\n");
+
+		// TODO: add proxy authentication here
+
+		connect.append("\r\n");
+		os.write(connect.toString().getBytes("UTF-8"));
+	}
+
+	/*
+	 HTTP/1.0 200 Connection established
+    Proxy-agent: Netscape-Proxy/1.1
+	 */
+	// TODO: methods needs cleanup / rewrite
+	private void verifyConnection() throws IOException {
+		InputStream is = super.getInputStream();
+
+		// read the status code
+		byte[] data = new byte[HTTP_PREFIX.length()];
+		if (is.read(data) < data.length) {
+			throw new IOException("Unexpected end of stream while reading proxy answer.");
+		}
+
+		// verify the server response (was it a http-connect proxy and did the connection succeed?)
+		String answer = new String(data).toUpperCase();
+		if (!answer.startsWith("HTTP/") || answer.endsWith(" 200")) {
+			data = new byte[4096];
+			int size = is.read(data);
+			String error = answer + new String(data, 0, size);
+			throw new IOException("Proxy connection failed. Proxy response: " + error);
+		}
+
+		// OK, the status code of the proxy was 200 Connection established
+		// so now we have to skip until the end of the proxy header (CR LF CR LF)
+		long startTime = System.currentTimeMillis();
+		int exspected = 0;
+		while (startTime + TIMEOUT * 1000 > System.currentTimeMillis()) {
+			int b = is.read();
+			if (b < 0) {
+				throw new IOException("Unexpected end of stream while reading proxy preload.");
+			}
+
+			// Check whether this byte is exspected in the byte sequence
+			if (b == HTTP_SUFFIX[exspected]) {
+				exspected++;
+			} else {
+				exspected = 0;
+			}
+
+			// Did we find the complete byte sequence?
+			if (exspected == HTTP_SUFFIX.length) {
+				return;
+			}
+		}
+
+		// TODO: include server response here
+		throw new IOException("Timeout while reading proxy preload");
+	}
+
+	
+}
