5e1a885f7a2a8956b212723ced3e6b409132faa9
hendrik <nhnb@users.sourceforge.net>
2007-10-22 21:19:23 +0000

    added some empty lines and some . at the end of sentences

--- src/marauroa/common/game/Result.java
+++ src/marauroa/common/game/Result.java
@@ -7,27 +7,35 @@ package marauroa.common.game;
  * @author miguel
  */
 public enum Result {
+
 	/** Account was created correctly. */
 	OK_CREATED(true, "Account was created correctly."),
+
 	/**
 	 * Account was not created because one of the important parameters was
 	 * missing.
 	 */
 	FAILED_EMPTY_STRING(false, "Account was not created because one of the important parameters was missing."),
+
 	/**
-	 * Account was not created because an invalid characters( letter, sign,
-	 * number ) was used
+	 * Account was not created because an invalid characters (letter, sign,
+	 * number) was used
 	 */
-	FAILED_INVALID_CHARACTER_USED (false, "Account was not created because an invalid characters( letter, sign, number ) was used"),
+	FAILED_INVALID_CHARACTER_USED (false, "Account was not created because an invalid characters (letter, sign, number) was used."),
+
 	/**
 	 * Account was not created because any of the parameters are either too long
 	 * or too short.
 	 */
-	FAILED_STRING_SIZE(false, "Account was not created because any of the parameters are either too long or too short"),
+	FAILED_STRING_SIZE(false, "Account was not created because any of the parameters are either too long or too short."),
+
 	/** Account was not created because because this account already exists. */
-	FAILED_PLAYER_EXISTS(false, "Account was not created because because this account already exists"),
+
+	FAILED_PLAYER_EXISTS(false, "Account was not created because because this account already exists."),
+
 	/** Account was not created because there was an unspecified exception. */
 	FAILED_EXCEPTION(false, "Account was not created because there was an unspecified exception."),
+
 	/**
 	 * The template passed to the create character method is not valid because
 	 * it fails to pass the RP rules.
@@ -38,6 +46,7 @@ public enum Result {
 	 * Textual description of the result 
 	 */
 	private String text;
+
 	/**
 	 * True if the account is successfully created. 
 	 */
