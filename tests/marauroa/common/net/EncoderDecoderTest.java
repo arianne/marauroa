6173534c38395092d89ba0991cc75340aa3d6633
miguel <arianne_rpg@users.sourceforge.net>
2007-11-24 18:08:40 +0000

    Removed unused imports.

--- tests/marauroa/common/net/EncoderDecoderTest.java
+++ tests/marauroa/common/net/EncoderDecoderTest.java
@@ -1,9 +1,9 @@
 package marauroa.common.net;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
