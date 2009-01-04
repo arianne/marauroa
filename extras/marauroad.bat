cffccad7492fde67bc689cdd17c8ff279357cf6c
astridemma <astridemma@users.sourceforge.net>
2009-01-04 10:55:58 +0000

    fixing [ 2485101 ] marauroad.bat bug

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-2.5.1.jar;libs/jython.jar;libs/mysql-connector.jar;libs/log4j.jar
+set LOCALCLASSPATH=.;libs/marauroa-2.6.jar;libs/jython.jar;libs/mysql-connector.jar;libs/log4j.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c server.ini -l
