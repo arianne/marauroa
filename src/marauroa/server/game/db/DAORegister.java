c26e3497d7c6b8407ce4264818fde8f52a83cfad
hendrik <nhnb@users.sourceforge.net>
2009-07-05 18:56:38 +0000

    registry for DAOs

---
+++ src/marauroa/server/game/db/DAORegister.java
@@ -0,0 +1,59 @@
+package marauroa.server.game.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * registers data access objects, so that they can be overriden by a user of the framework.
+ *
+ * @author hendrik
+ */
+public class DAORegister {
+
+	private Map<Class<?>, Object> register = new HashMap<Class<?>, Object>();
+	private DAORegister instance;
+
+	private DAORegister() {
+		// hide constructor, this is a Singleton
+	}
+
+	/**
+	 * gets the singleton DAORegister instance
+	 *
+	 * @return DAORegister
+	 */
+	public DAORegister get() {
+		if (instance == null) {
+			instance = new DAORegister();
+		}
+		return instance;
+	}
+
+	/**
+	 * registers a DAO
+	 *
+	 * @param <T>   type of DOA
+	 * @param clazz class of DOA
+	 * @param object instance of DOA
+	 */
+	public <T> void register(Class<T> clazz, T object) {
+		register.put(clazz, object);
+	}
+
+	/**
+	 * gets the instance for the requested DAO
+	 *
+	 * @param <T>   type of DAO
+	 * @param clazz class of DAP
+	 * @return instance of DOA
+	 * @throws IllegalArgumentException in case there is no instance registered for the specified class
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T get(Class<T> clazz) {
+		T res = (T) register.get(clazz);
+		if (res == null) {
+			throw new IllegalArgumentException("No DOA registered for class " + clazz);
+		}
+		return res;
+	}
+}
