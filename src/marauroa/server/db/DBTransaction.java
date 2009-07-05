b129c2c21201403745add57941bf263bf90a1f9a
hendrik <nhnb@users.sourceforge.net>
2009-07-05 15:40:50 +0000

    ported method "subst" from webflag to marauroa and hereby relicensed as GPL by its author (me)

--- src/marauroa/server/db/DBTransaction.java
+++ src/marauroa/server/db/DBTransaction.java
@@ -1,10 +1,19 @@
 package marauroa.server.db;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import marauroa.common.Log4J;
 import marauroa.common.Logger;
 import marauroa.server.db.adapter.DatabaseAdapter;
+import marauroa.server.game.db.StringChecker;
+
+import com.sun.org.apache.regexp.internal.RE;
+import com.sun.org.apache.regexp.internal.RESyntaxException;
 
 /**
  * a database transaction
@@ -12,9 +21,13 @@ import marauroa.server.db.adapter.DatabaseAdapter;
  * @author hendrik
  */
 public class DBTransaction {
-    private static Logger logger = Log4J.getLogger(TransactionPool.class);
+    private static Logger logger = Log4J.getLogger(DBTransaction.class);
 
 	private DatabaseAdapter databaseAdapter = null;
+	private LinkedList<Statement> statements = null;
+	private LinkedList<ResultSet> resultSets = null;
+    private RE reInt;
+    private RE reIntList;
 
 	/**
 	 * creates a new DBTransaction
@@ -23,6 +36,12 @@ public class DBTransaction {
 	 */
 	protected DBTransaction(DatabaseAdapter databaseAdapter) {
 		this.databaseAdapter = databaseAdapter;
+        try {
+            reInt = new RE("^[0-9 ]*$");
+            reIntList = new RE("^[0-9, ]*$");
+        } catch (RESyntaxException e) {
+            logger.error(e, e);
+        }
 	}
 
 	/**
@@ -49,4 +68,55 @@ public class DBTransaction {
 			logger.error(e, e);
 		}
 	}
+
+
+    /**
+     * Replaces variables SQL-Statements and prevents SQL injection attacks
+     *
+     * @param sql SQL-String
+     * @param params replacement parameters
+     * @return SQL-String with substitued parameters
+     * @throws SQLException in case of an sql injection attack
+     */
+    public String subst(String sql, Map<String, ?> params) throws SQLException {
+        StringBuffer res = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(sql, "([]'", true);
+        String lastToken = "";
+        String secondLastToken = "";
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if (lastToken.equals("[")) {
+
+                // Variablen ersetzen
+                Object temp = params.get(token);
+                if (temp != null) {
+                    token = temp.toString();
+                } else {
+                    token = "";
+                }
+
+                // SQL-Injection abfangen
+                if (secondLastToken.equals("(")) {
+                    if (!reIntList.match(token)) {
+                        throw new SQLException("Illegal argument: \"" + token + "\" is not an integer list"); 
+                    }
+                } else if (secondLastToken.equals("'")) {
+                    if (token.length() > 0) {
+                        token = StringChecker.escapeSQLString(token);
+                    }
+                } else {
+                    if (!reInt.match(token)) {
+                        throw new SQLException("Illegal argument: \"" + token + "\" is not an integer."); 
+                    }
+                }
+            }
+            secondLastToken = lastToken;
+            lastToken = token.trim();
+            if (token.equals("[") || token.equals("]")) {
+                token = "";
+            }
+            res.append(token);
+        }
+        return res.toString();
+    }	
 }
