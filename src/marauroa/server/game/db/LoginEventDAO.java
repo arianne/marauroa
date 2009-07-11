5f9e948f07cf39ceaf32c81eb6f5ae52e9766bd3
hendrik <nhnb@users.sourceforge.net>
2009-07-11 22:32:21 +0000

    implemented getLastSuccessfulLoginEvent and getAmountOfFailedLogins

--- src/marauroa/server/game/db/LoginEventDAO.java
+++ src/marauroa/server/game/db/LoginEventDAO.java
@@ -1,4 +1,4 @@
-/* $Id: LoginEventDAO.java,v 1.6 2009/07/11 21:11:40 nhnb Exp $ */
+/* $Id: LoginEventDAO.java,v 1.7 2009/07/11 22:32:21 nhnb Exp $ */
 /***************************************************************************
  *                   (C) Copyright 2003-2009 - Marauroa                    *
  ***************************************************************************
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 import marauroa.common.Log4J;
+import marauroa.common.Pair;
 import marauroa.server.db.DBTransaction;
 import marauroa.server.db.TransactionPool;
 
@@ -32,8 +33,7 @@ import marauroa.server.db.TransactionPool;
 public class LoginEventDAO {
 	private static final marauroa.common.Logger logger = Log4J.getLogger(LoginEventDAO.class);
 
-	public void addLoginEvent(DBTransaction transaction, String username, InetAddress source,
-	        boolean correctLogin) throws SQLException {
+	public void addLoginEvent(DBTransaction transaction, String username, InetAddress source, boolean correctLogin) throws SQLException {
 
 		try {
 			int id = DAORegister.get().get(AccountDAO.class).getDatabasePlayerId(transaction, username);
@@ -42,7 +42,7 @@ public class LoginEventDAO {
 			// and bruteforcing matching usernames.
 
 			String query = "insert into loginEvent(player_id, address, timedate, result)"
-				+ "values([player_id], '[address]', NULL, [result])";
+				+ " values ([player_id], '[address]', NULL, [result])";
 			Map<String, Object> params = new HashMap<String, Object>();
 			params.put("player_id", Integer.valueOf(id));
 			params.put("address", source.getHostAddress());
@@ -128,7 +128,63 @@ public class LoginEventDAO {
 		}
 	}
 
+	public LoginEvent getLastSuccessfulLoginEvent(DBTransaction transaction, int playerId, String service) throws SQLException {
+		try {
+			String serviceQuery = "";
+			if (service != null) {
+				serviceQuery = " AND service='[service]'";
+			}
+			String query = "SELECT id, player_id, service, address, timedate, result FROM loginEvent"
+				+ " WHERE player_id=[player_id] AND result=1" + serviceQuery + " ORDER BY timedate desc LIMIT 2";
 
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("service", service);
+			params.put("player_id", Integer.toString(playerId));
+			
+			logger.debug("getLastSuccessfulLoginEvent is executing query " + query);
+
+			ResultSet resultSet = transaction.query(query, params);
+
+			// go to this login
+			if (!resultSet.next()) {
+				return null;
+			}
+			// go to the last login
+			if (!resultSet.next()) {
+				return null;
+			}
+			
+			LoginEvent event = new LoginEvent(/*TODO:  resultSet.getLong("id"), 
+					resultSet.getLong("player_id"), resultSet.getString("service"),*/
+					resultSet.getString("address"), resultSet.getString("timedate"),
+					resultSet.getBoolean("result"));
+			return event;
+		} catch (SQLException e) {
+			logger.error("Can't query for player \"" + playerId + "\"", e);
+			throw e;
+		}
+	}	
+
+	public List<Pair<String, Long>> getAmountOfFailedLogins(DBTransaction transaction, long id, int playerId, String service) throws SQLException {
+		try {
+			String query = "SELECT service, count(*) FROM loginEvent"
+				+ " WHERE player_id=[player_id] + AND id > [id] AND result = 0 GROUP BY service";
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("id", Long.toString(id));
+			params.put("player_id", Integer.toString(playerId));
+			logger.debug("getAmountOfFailedLogins is executing query " + query);
+		
+			ResultSet resultSet = transaction.query(query, params);
+			List<Pair<String, Long>> list = new LinkedList<Pair<String, Long>>();
+			while (resultSet.next()) {
+				list.add(new Pair<String, Long>(resultSet.getString(1), Long.valueOf(resultSet.getLong(2))));
+			}
+			return list;
+		} catch (SQLException e) {
+			logger.error("Can't query for player \"" + playerId + "\"", e);
+			throw e;
+		}
+	}	
 
 	public void addLoginEvent(String username, InetAddress source, boolean correctLogin) throws SQLException {
 		DBTransaction transaction = TransactionPool.get().beginWork();
