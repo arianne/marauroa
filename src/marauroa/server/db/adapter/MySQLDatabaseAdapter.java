93bab2cbc439461f5cf97ef207893e4e1e1b7e24
hendrik <nhnb@users.sourceforge.net>
2009-07-11 14:49:06 +0000

    added license template to new files

--- src/marauroa/server/db/adapter/MySQLDatabaseAdapter.java
+++ src/marauroa/server/db/adapter/MySQLDatabaseAdapter.java
@@ -1,3 +1,15 @@
+/* $Id: MySQLDatabaseAdapter.java,v 1.6 2009/07/11 14:49:06 nhnb Exp $ */
+/***************************************************************************
+ *                   (C) Copyright 2007-2009 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package marauroa.server.db.adapter;
 
 import java.io.IOException;
