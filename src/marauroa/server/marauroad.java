4e53927cde6b53e5c75979abe927406467de18b8
hendrik <nhnb@users.sourceforge.net>
2009-11-12 21:47:02 +0000

    increased version number

--- src/marauroa/server/marauroad.java
+++ src/marauroa/server/marauroad.java
@@ -1,4 +1,4 @@
-/* $Id: marauroad.java,v 1.85 2009/10/18 20:42:31 nhnb Exp $ */
+/* $Id: marauroad.java,v 1.86 2009/11/12 21:47:02 nhnb Exp $ */
 /***************************************************************************
  *						(C) Copyright 2003 - Marauroa					   *
  ***************************************************************************
@@ -154,7 +154,7 @@ public class marauroad extends Thread {
 	private static final Logger logger = Log4J.getLogger(marauroad.class);
 
 	/** Which marauroa version are we running */
-	private static final String VERSION = "3.2.1";
+	private static final String VERSION = "3.2.2";
 
 	/** Marauroa is a singleton. */
 	private static marauroad marauroa;
