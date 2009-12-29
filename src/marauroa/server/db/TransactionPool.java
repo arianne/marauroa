923c7f566340c7ad0d5f5c19e54ff1111fabccca
hendrik <nhnb@users.sourceforge.net>
2009-12-29 12:31:10 +0000

    code format (no other changes)

--- src/marauroa/server/db/TransactionPool.java
+++ src/marauroa/server/db/TransactionPool.java
@@ -21,187 +21,186 @@ import marauroa.common.Pair;
  * @author hendrik
  */
 public class TransactionPool {
-    private static Logger logger = Log4J.getLogger(TransactionPool.class);
-    private static TransactionPool dbtransactionPool = null;
-    private AdapterFactory factory = null;
-    private Object wait = new Object();
-    private Properties params = new Properties();
-    private int count = 10;
 
-    private List<DBTransaction> dbtransactions = Collections.synchronizedList(new LinkedList<DBTransaction>()); 
-    private List<DBTransaction> freeDBTransactions = Collections.synchronizedList(new LinkedList<DBTransaction>());
-    private ThreadLocal<Set<DBTransaction>> threadTransactions = new ThreadLocal<Set<DBTransaction>>();
-    private Map<DBTransaction, Pair<String, StackTraceElement[]>> callers;
+	private static Logger logger = Log4J.getLogger(TransactionPool.class);
+	private static TransactionPool dbtransactionPool = null;
+	private AdapterFactory factory = null;
+	private Object wait = new Object();
+	private Properties params = new Properties();
+	private int count = 10;
+	private List<DBTransaction> dbtransactions = Collections.synchronizedList(new LinkedList<DBTransaction>());
+	private List<DBTransaction> freeDBTransactions = Collections.synchronizedList(new LinkedList<DBTransaction>());
+	private ThreadLocal<Set<DBTransaction>> threadTransactions = new ThreadLocal<Set<DBTransaction>>();
+	private Map<DBTransaction, Pair<String, StackTraceElement[]>> callers;
+	private boolean closed = false;
 
-	private boolean closed = false; 
+	/**
+	 * creates a DBTransactionPool
+	 *
+	 * @param connfiguration connfiguration
+	 */
+	public TransactionPool(Properties connfiguration) {
+		params = connfiguration;
+		count = Integer.parseInt(params.getProperty("count", "4"));
+		callers = Collections.synchronizedMap(new HashMap<DBTransaction, Pair<String, StackTraceElement[]>>());
+		factory = new AdapterFactory(connfiguration);
+	}
 
-    /**
-     * creates a DBTransactionPool
-     *
-     * @param connfiguration connfiguration
-     */
-    public TransactionPool(Properties connfiguration) {
-    	params = connfiguration;
-        count = Integer.parseInt(params.getProperty("count", "4"));
-        callers = Collections.synchronizedMap(new HashMap<DBTransaction, Pair<String, StackTraceElement[]>>()); 
-        factory = new AdapterFactory(connfiguration);
-    }
-    
-    /**
-     * registers this TransactionPool as the global one.
-     */
-    public void registerGlobally() {
-        registerGlobal(this);
-    }
+	/**
+	 * registers this TransactionPool as the global one.
+	 */
+	public void registerGlobally() {
+		registerGlobal(this);
+	}
 
-    /**
-     * registers a TransactionPool as the global one.
-     *
-     * @param dbtransactionPool the pool to register globally
-     */
-    private static void registerGlobal(TransactionPool dbtransactionPool) {
-        TransactionPool.dbtransactionPool = dbtransactionPool;
-    }
+	/**
+	 * registers a TransactionPool as the global one.
+	 *
+	 * @param dbtransactionPool the pool to register globally
+	 */
+	private static void registerGlobal(TransactionPool dbtransactionPool) {
+		TransactionPool.dbtransactionPool = dbtransactionPool;
+	}
 
-    /**
-     * gets the TransactionPool
-     *
-     * @return TransactionPool
-     */
-    public static synchronized TransactionPool get() {
-        return dbtransactionPool;
-    }
+	/**
+	 * gets the TransactionPool
+	 *
+	 * @return TransactionPool
+	 */
+	public static synchronized TransactionPool get() {
+		return dbtransactionPool;
+	}
 
-    private void createMinimumDBTransactions() {
-        synchronized (wait) {    	
-	        while (dbtransactions.size() < count) {
-	            DBTransaction dbtransaction = new DBTransaction(factory.create());
-	            dbtransactions.add(dbtransaction);
-	            freeDBTransactions.add(dbtransaction);
-	        }
-        }
-    }
+	private void createMinimumDBTransactions() {
+		synchronized (wait) {
+			while (dbtransactions.size() < count) {
+				DBTransaction dbtransaction = new DBTransaction(factory.create());
+				dbtransactions.add(dbtransaction);
+				freeDBTransactions.add(dbtransaction);
+			}
+		}
+	}
 
-    /**
-     * starts a transaction and marks it as reserved
-     *
-     * @return DBTransaction
-     */
-    public DBTransaction beginWork() {
-    	if (closed) {
-    		throw new RuntimeException("transaction pool has been closed");
-    	}
-        DBTransaction dbtransaction = null;
-        while (dbtransaction == null) {
-            synchronized (wait) {
-                createMinimumDBTransactions();                
-                while (freeDBTransactions.size() == 0) {
-                    createMinimumDBTransactions();
-                    try {
-                        logger.info("Waiting for a DBTransaction", new Throwable());
-                        dumpOpenTransactions();
-                        wait.wait();
-                    } catch (InterruptedException e) {
-                        logger.error(e, e);
-                    }
-                }
+	/**
+	 * starts a transaction and marks it as reserved
+	 *
+	 * @return DBTransaction
+	 */
+	public DBTransaction beginWork() {
+		if (closed) {
+			throw new RuntimeException("transaction pool has been closed");
+		}
+		DBTransaction dbtransaction = null;
+		while (dbtransaction == null) {
+			synchronized (wait) {
+				createMinimumDBTransactions();
+				while (freeDBTransactions.size() == 0) {
+					createMinimumDBTransactions();
+					try {
+						logger.info("Waiting for a DBTransaction", new Throwable());
+						dumpOpenTransactions();
+						wait.wait();
+					} catch (InterruptedException e) {
+						logger.error(e, e);
+					}
+				}
 
-                dbtransaction = freeDBTransactions.remove(0);
-                addThreadTransaction(dbtransaction);
-                // TODO: check that the connection is still alive
-            }
-        }
-        logger.debug("getDBTransaction: " + dbtransaction, new Throwable());
-        
-        Thread currentThread = Thread.currentThread(); 
-        callers.put(dbtransaction, new Pair<String, StackTraceElement[]>(currentThread.getName(), currentThread.getStackTrace()));
-        return dbtransaction;
-    }
+				dbtransaction = freeDBTransactions.remove(0);
+				addThreadTransaction(dbtransaction);
+				// TODO: check that the connection is still alive
+			}
+		}
+		logger.debug("getDBTransaction: " + dbtransaction, new Throwable());
 
-    /**
-     * dumps a list of open transactions with their threads and stacktraces to the log file.
-     */
+		Thread currentThread = Thread.currentThread();
+		callers.put(dbtransaction, new Pair<String, StackTraceElement[]>(currentThread.getName(), currentThread.getStackTrace()));
+		return dbtransaction;
+	}
+
+	/**
+	 * dumps a list of open transactions with their threads and stacktraces to the log file.
+	 */
 	public void dumpOpenTransactions() {
 		for (Pair<String, StackTraceElement[]> pair : callers.values()) {
 			logger.info("      * " + pair.first() + " " + Arrays.asList(pair.second()));
 		}
 	}
 
-    /**
-     * commits this transaction and frees it reservation
-     *
-     * @param dbtransaction transaction
-     * @throws SQLException in case of an database error
-     */
-    public void commit(DBTransaction dbtransaction) throws SQLException {
-    	try {
+	/**
+	 * commits this transaction and frees it reservation
+	 *
+	 * @param dbtransaction transaction
+	 * @throws SQLException in case of an database error
+	 */
+	public void commit(DBTransaction dbtransaction) throws SQLException {
+		try {
 			dbtransaction.commit();
 		} catch (SQLException e) {
 			freeDBTransaction(dbtransaction);
 			throw e;
 		}
-    	freeDBTransaction(dbtransaction);
-    }
+		freeDBTransaction(dbtransaction);
+	}
 
-    /**
-     * rolls this transaction back and frees the reservation
-     *
-     * @param dbtransaction transaction
-     */
-    public void rollback(DBTransaction dbtransaction) {
-    	dbtransaction.rollback();
-    	freeDBTransaction(dbtransaction);
-    }
-    
-    private void freeDBTransaction(DBTransaction dbtransaction) {
-        logger.debug("freeDBTransaction: " + dbtransaction, new Throwable());
-        synchronized (wait) {
-	        threadTransactions.get().remove(dbtransaction);
-	        callers.remove(dbtransaction);
-	        if (dbtransactions.contains(dbtransaction)) {
-	            freeDBTransactions.add(dbtransaction);
-	        } else {
-	            logger.error("Unbekannter DBTransaction " + dbtransaction + " nicht freigegeben.", new Throwable());
-	        }
-	        wait.notifyAll();
-        }
-    }
+	/**
+	 * rolls this transaction back and frees the reservation
+	 *
+	 * @param dbtransaction transaction
+	 */
+	public void rollback(DBTransaction dbtransaction) {
+		dbtransaction.rollback();
+		freeDBTransaction(dbtransaction);
+	}
 
-    private void addThreadTransaction(DBTransaction dbtransaction) {
-    	Set<DBTransaction> set = threadTransactions.get();
-    	if (set == null) {
-    		set = new HashSet<DBTransaction>();
-    		threadTransactions.set(set);
-    	}
-    	set.add(dbtransaction);
-    }
+	private void freeDBTransaction(DBTransaction dbtransaction) {
+		logger.debug("freeDBTransaction: " + dbtransaction, new Throwable());
+		synchronized (wait) {
+			threadTransactions.get().remove(dbtransaction);
+			callers.remove(dbtransaction);
+			if (dbtransactions.contains(dbtransaction)) {
+				freeDBTransactions.add(dbtransaction);
+			} else {
+				logger.error("Unbekannter DBTransaction " + dbtransaction + " nicht freigegeben.", new Throwable());
+			}
+			wait.notifyAll();
+		}
+	}
 
-    /**
-     * Kicks all transaction which where started in the current thread
-     */
-    public void kickHangingTransactionsOfThisThread() {
-    	Set<DBTransaction> set = threadTransactions.get();
-    	if ((set == null) || set.isEmpty()) {
-    		return;
-    	}
+	private void addThreadTransaction(DBTransaction dbtransaction) {
+		Set<DBTransaction> set = threadTransactions.get();
+		if (set == null) {
+			set = new HashSet<DBTransaction>();
+			threadTransactions.set(set);
+		}
+		set.add(dbtransaction);
+	}
 
-        synchronized (wait) {
-        	for (DBTransaction dbtransaction : set) {
-	    		dbtransaction.rollback();
-	    		dbtransaction.close();
-	    		dbtransactions.remove(dbtransaction);
-	    		callers.remove(dbtransaction);
-	    		logger.error("Hanging transaction " + dbtransaction + " was kicked.");
-        	}
-    	}
-    	set.clear();
-    }
+	/**
+	 * Kicks all transaction which where started in the current thread
+	 */
+	public void kickHangingTransactionsOfThisThread() {
+		Set<DBTransaction> set = threadTransactions.get();
+		if ((set == null) || set.isEmpty()) {
+			return;
+		}
 
-    /**
-     * closes the transaction pool
-     */
+		synchronized (wait) {
+			for (DBTransaction dbtransaction : set) {
+				dbtransaction.rollback();
+				dbtransaction.close();
+				dbtransactions.remove(dbtransaction);
+				callers.remove(dbtransaction);
+				logger.error("Hanging transaction " + dbtransaction + " was kicked.");
+			}
+		}
+		set.clear();
+	}
+
+	/**
+	 * closes the transaction pool
+	 */
 	public void close() {
-		closed  = true;
+		closed = true;
 		for (DBTransaction transaction : dbtransactions) {
 			transaction.close();
 		}
