88419fe8605535d0b0232c9a0d17d11221a4eade
hendrik <nhnb@users.sourceforge.net>
2010-01-31 20:45:07 +0000

    add license boiler plate

--- tests/marauroa/common/game/RPClassTestHelper.java
+++ tests/marauroa/common/game/RPClassTestHelper.java
@@ -1,3 +1,15 @@
+/* $Id: RPClassTestHelper.java,v 1.2 2010/01/31 20:45:11 nhnb Exp $ */
+/***************************************************************************
+ *                   (C) Copyright 2007-2010 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package marauroa.common.game;
 
 import marauroa.common.game.Definition.Type;
--- tests/marauroa/server/db/DBTransactionTest.java
+++ tests/marauroa/server/db/DBTransactionTest.java
@@ -1,3 +1,15 @@
+/* $Id: DBTransactionTest.java,v 1.2 2010/01/31 20:45:07 nhnb Exp $ */
+/***************************************************************************
+ *                   (C) Copyright 2007-2010 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package marauroa.server.db;
 
 import static org.hamcrest.CoreMatchers.equalTo;
