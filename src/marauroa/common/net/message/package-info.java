7ace83d3d573f642de29d7dfb42f8e5aac6cca80
hendrik <nhnb@users.sourceforge.net>
2010-02-25 20:09:54 +0000

    use message images in javadoc

--- src/marauroa/common/net/message/package-info.java
+++ src/marauroa/common/net/message/package-info.java
@@ -4,6 +4,12 @@
  * Each message is responsible to serialize and deserialize itself on request.
  * <p>
  * There is no implementation of versioning. 
+ * <ul>
+ * <li>doc-files/message-connected.png</li>
+ * <li>doc-files/message-loggedin.png</li>
+ * <li>doc-files/message-game.png</li>
+ * <li>doc-files/message-logout.png</li>
+ * </ul>
  */
 package marauroa.common.net.message;
 
