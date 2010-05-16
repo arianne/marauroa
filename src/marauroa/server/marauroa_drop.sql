790924a61e1779aa19eb8e0ab6bf6f70f039969c
astridemma <astridemma@users.sourceforge.net>
2010-05-16 17:03:28 +0000

    changed drop statements to drop all tables created by marauroa_init.sql

--- src/marauroa/server/marauroa_drop.sql
+++ src/marauroa/server/marauroa_drop.sql
@@ -1,10 +1,13 @@
-drop table if exists player;
+drop table if exists account;
+drop table if exists accountban;
 drop table if exists characters;
 drop table if exists rpobject;
-drop table if exists rpattribute;
 drop table if exists rpslot;
 
 drop table if exists loginEvent;
+drop table if exists loginseed;
+drop table if exists passwordChange;
+drop table if exists rpzone;
 drop table if exists statistics;
 drop table if exists gameEvents;
 
