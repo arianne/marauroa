6ad4514803f3b9caa53a3325b5fcad32bce5eb6e
hendrik <nhnb@users.sourceforge.net>
2011-01-08 17:17:07 +0100

    UML diagram to explain the internal working of the server side network code

--- src/marauroa/server/net/package-info.java
+++ src/marauroa/server/net/package-info.java
@@ -1,8 +1,7 @@
 /**
- * The package stores the network related code.
- * <p>
- * It defines the interfaces for implementing a network manager ({@link marauroa.server.net.INetworkServerManager}) 
- * and the interface for implement a disconnection listener ({@link marauroa.server.net.IDisconnectedListener})
+ * Network communication.
+ *
+ * <img src="doc-files/net.png">
  */
 package marauroa.server.net;
 
