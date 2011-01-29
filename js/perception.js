a98d54987cf84588597163b33d5278610e1830b9
hendrik <nhnb@users.sourceforge.net>
2011-01-29 17:42:37 +0100

    implemented perception handler in JavaScript

--- js/perception.js
+++ js/perception.js
@@ -109,4 +109,146 @@ marauroa.perceptionListener = {
 	onException: function(exception, perception) {
 	}
 }
+marauroa.currentZone = {}
+marauroa.perceptionHandler = {
 
+	/**
+	 * Apply a perception.
+	 * 
+	 * @param message
+	 *            the perception message
+	 */
+	apply: function(perceptionmessage) {
+		marauroa.perceptionListener.onPerceptionBegin(message.sync, message.s);
+
+		// clean world on login/zone change
+		if (message.sync) {
+			if (!marauroa.perceptionListener.onClear()) {
+				marauroa.currentZone.clear();
+			}
+		}
+
+		// apply perception
+		applyPerceptionAddedRPObjects(message);
+		applyPerceptionModifiedRPObjects(message);
+		applyPerceptionDeletedRPObjects(message);
+		applyPerceptionMyRPObject(message);
+
+		// done, tell marauroa.perceptionListener
+		marauroa.perceptionListener.onPerceptionEnd(message.sync, message.s);
+	}
+
+	/**
+	 * This method applys perceptions addedto the Map<RPObject::ID,RPObject>
+	 * passed as argument. It clears the map if this is a sync perception
+	 * 
+	 * @param message
+	 *            the perception message
+	 */
+	private void applyPerceptionAddedRPObjects(MessageS2CPerception message) {
+		if (message.aO) {
+			for (i in message.aO) {
+				if (!marauroa.perceptionListener.onAdded(message.aO[i])) {
+					marauroa.currentZone[i] = message.aO[i];
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method applys perceptions deleted to the Map<RPObject::ID,RPObject>
+	 * passed as argument.
+	 * 
+	 * @param message
+	 *            the perception message
+	 */
+	private void applyPerceptionDeletedRPObjects(MessageS2CPerception message) {
+		if (message.dO) {
+			for (i in message.dO) {
+				if (!marauroa.perceptionListener.onDeleted(message.dO[i])) {
+					marauroa.currentZone[i] = undefined;
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method applies perceptions modified added and modified deleted to the
+	 * Map<RPObject.ID,RPObject> passed as argument.
+	 * 
+	 * @param message
+	 *            the perception message
+	 */
+	private void applyPerceptionModifiedRPObjects(MessageS2CPerception message) {
+
+		// deleted attributes
+		if (message.dA) {
+			for (i in message.dA) {
+				if (typeof(marauroa.currentZone[i.id]) != "undefined") {
+					o = marauroa.currentZone[i.id];
+					if (!marauroa.perceptionListener.onModifiedDeleted(message.dA[i])) {
+						// TODO apply differences recursively
+					}
+				}
+			}
+		}
+
+		// added attributes
+		if (message.aA) {
+			for (i in message.aA) {
+				if (typeof(marauroa.currentZone[i.id]) != "undefined") {
+					o = marauroa.currentZone[i.id];
+					if (!marauroa.perceptionListener.onModifiedAdded(message.dA[i])) {
+						// TODO apply differences recursively
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method applys perceptions for our RPObject to the Map<RPObject::ID,RPObject>
+	 * passed as argument.
+	 * 
+	 * @param message
+	 *            the perception message
+	 */
+	private void applyPerceptionMyRPObject(MessageS2CPerception message) {
+
+		addMyRPObjectToWorldIfPrivate(added);
+
+		if (!marauroa.perceptionListener.onMyRPObject(message.aM, message.dM)) {
+			if (typeof(message.aM) != "undefined") {
+				id = message.aM.id;
+			}
+
+			if (typeof(message.dM) != "undefined") {
+				id = message.dM.id;
+			}
+
+			if (typeof(id) == "undefined") {
+				return;
+			}
+
+			object = marauroa.currentZone[id];
+			// TODO: object.applyDifferences(added, deleted);
+		}
+	}
+
+	/**
+	 * adds our RPObject to the world in case it was not already added by the public perception.
+	 *
+	 * @param added added changes of my object
+	 */
+	private void addMyRPObjectToWorldIfPrivate(RPObject added) {
+		if (typeof(added) == "undefined") {
+			return;
+		}
+		if (typeof(marauroa.currentZone[added.id]) != "undefined") {
+			return;
+		}
+		if (!marauroa.perceptionListener.onAdded(added)) {
+			marauroa.currentZone[added.id] = added;
+		}
+	}
+}
