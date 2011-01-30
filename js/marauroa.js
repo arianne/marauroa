f029751148c4fb41d7792f6e7124764ca133b21f
hendrik <nhnb@users.sourceforge.net>
2011-01-30 13:30:25 +0100

    added marauroa.log.{debug, info, warn, error) to log to firebug. If firebug is not installed, error will open an alert box and the rest is ignored

--- js/marauroa.js
+++ js/marauroa.js
@@ -1,1 +1,7 @@
 var marauroa = new function() {}
+
+marauroa.log = {};
+marauroa.log.debug = console.debug || function(text) {};
+marauroa.log.info = console.info || function(text) {};
+marauroa.log.warn = console.warn || function(text) {};
+marauroa.log.error = console.error || function(text) {alert(text)};
