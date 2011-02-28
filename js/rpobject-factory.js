ab5e9d5c5f1b0ca7e06cc4baa4dc1e44e519442e
hendrik <nhnb@users.sourceforge.net>
2011-02-28 20:37:52 +0100

    _default object can now be used as prototype

--- js/rpobject-factory.js
+++ js/rpobject-factory.js
@@ -1,15 +1,15 @@
-marauroa.rpobjectFactory = {
-	createRPObject: function(rpclass) {
+marauroa.rpobjectFactory = new function(){
+	this._default = function() {};
+	this._default.onEvent = function(e) {
+		marauroa.log.debug("Event: " + JSON.stringify(e));
+	}
+
+	this.createRPObject = function(rpclass) {
 		var ctor = this._default;
 		if (typeof(this[rpclass]) != "undefined") {
 			ctor = this[rpclass];
 		}
 		return new ctor;
-	},
-
-	_default: function() {
-		this.onEvent = function(e) {
-			marauroa.log.debug("Event: " + JSON.stringify(e));
-		}
 	}
+
 }
