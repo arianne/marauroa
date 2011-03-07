676a3d61bd3d8f1ffae86b9a136f0076bc5dbc9a
hendrik <nhnb@users.sourceforge.net>
2011-03-07 08:20:12 +0100

    implemented marauroa.rpeventFactory

--- js/rpfactory.js
+++ js/rpfactory.js
@@ -6,11 +6,60 @@
 marauroa.rpobjectFactory = new function(){
 	this._default = function() {};
 	this._default.onEvent = function(e) {
-		marauroa.log.debug("Event: ", e);
+		var event = marauroa.rpeventFactory.create(e.c);
+		for (var i in e.a) {
+			if (e.a.hasOwnProperty(i)) {
+				event[i] = e.a[i];
+			}
+			event._rpclass = e.c;
+		}
+		event.execute(this);
 	}
 	this._default.set = function(key, value) {
 		this[key] = value;
 	}
+
+	this.create = function(rpclass) {
+		var ctor = this._default;
+		if (typeof(this[rpclass]) != "undefined") {
+			ctor = this[rpclass];
+		}
+		return marauroa.util.fromProto(ctor);
+	}
+}
+
+
+
+
+/**
+ * creates RPEvent
+ *
+ * use the rpclass name as attribute name for a prototype object
+ */
+marauroa.rpeventFactory = new function(){
+	this._default = function() {};
+	this._default.execute = function(rpobject) {
+		marauroa.log.debug("Unhandled event: ", this, " on ", rpobject);
+	}
+
+	this.create = function(rpclass) {
+		var ctor = this._default;
+		if (typeof(this[rpclass]) != "undefined") {
+			ctor = this[rpclass];
+		}
+		return marauroa.util.fromProto(ctor);
+	}
+}
+
+
+
+/**
+ * creates RPSlot
+ *
+ * use the rpclass name as attribute name for a prototype object
+ */
+marauroa.rpslotFactory = new function(){
+	this._default = function() {};
 
 	this.create = function(rpclass) {
 		var ctor = this._default;
