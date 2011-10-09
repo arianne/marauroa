d5f29c682c3fce05167693a28c6b93f6349a1132
hendrik <nhnb@users.sourceforge.net>
2011-10-09 18:30:12 +0200

    updated version number

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-3.8.7.jar;libs/jython.jar;libs/h2.jar;libs/mysql-connector.jar;libs/log4j.jar
+set LOCALCLASSPATH=.;marauroa-3.8.8.jar;libs/jython.jar;libs/h2.jar;libs/mysql-connector.jar;libs/log4j.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c server.ini -l
