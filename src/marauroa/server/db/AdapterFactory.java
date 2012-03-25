0bf9c6c7e84e5ec98285bec7c881437d6c5909d0
hendrik <nhnb@users.sourceforge.net>
2012-03-25 14:58:09 +0200

    throw exception on initally failed database connection, retry on interruption

--- src/marauroa/server/db/AdapterFactory.java
+++ src/marauroa/server/db/AdapterFactory.java
@@ -11,7 +11,9 @@
  ***************************************************************************/
 package marauroa.server.db;
 
+import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
 import marauroa.common.Configuration;
@@ -28,7 +30,8 @@ import marauroa.server.db.adapter.MySQLDatabaseAdapter;
 class AdapterFactory {
 	private static Logger logger = Log4J.getLogger(AdapterFactory.class);
 
-	private Properties connInfo;
+	private final Properties connInfo;
+	private boolean gotAConnection = false;
 
 	/**
 	 * creates a new AdapterFactory
@@ -40,24 +43,63 @@ class AdapterFactory {
 	}
 
 	/**
+	 * creates a database adapter
+	 *
+	 * @return database adapter
+	 * @throws IOException             in case of an unexpected input/output error
+	 * @throws ClassNotFoundException  if the adapter class is missing
+	 * @throws SecurityException       if a security manager prevented the operation
+	 * @throws NoSuchMethodException   if the constructor with a Properties object as parameter is missing
+	 * @throws IllegalArgumentException on an internal error
+	 * @throws InstantiationException   if the adapter class may not be instantiated
+	 * @throws IllegalAccessException   on an internal error
+	 * @throws InvocationTargetException if the constructor threw an exception
+	 */
+	private DatabaseAdapter internalCreate() throws IOException, ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		Configuration configuration = Configuration.getConfiguration();
+		String adapter = configuration.get("database_adapter");
+		if (adapter == null) {
+			return new MySQLDatabaseAdapter(connInfo);
+		}
+		Class<? extends DatabaseAdapter> clazz = Class.forName(adapter).asSubclass(DatabaseAdapter.class);
+		Constructor<? extends DatabaseAdapter> ctor = clazz.getConstructor(Properties.class);
+		return ctor.newInstance(connInfo);
+	}
+
+	/**
 	 * creates a DatabaseAdapter
 	 *
 	 * @return DatabaseAdapter for the specified database
 	 */
-	@SuppressWarnings("unchecked")
     public DatabaseAdapter create() {
+		Configuration configuration;
 		try {
-			Configuration configuration = Configuration.getConfiguration();
-			String adapter = configuration.get("database_adapter");
-			if (adapter == null) {
-				return new MySQLDatabaseAdapter(connInfo);
+			configuration = Configuration.getConfiguration();
+		} catch (IOException e) {
+			throw new RuntimeException("Could not read configuration:", e);
+		}
+		int maxRetries = configuration.getInt("database_connection_retries", 60);
+		int wait = configuration.getInt("database_connection_waittime", 1000);
+
+		int i = 0;
+		while (true) {
+			try {
+				DatabaseAdapter adapter = internalCreate();
+				gotAConnection = true;
+				return adapter;
+			} catch (Exception e) {
+				if (gotAConnection && (i < maxRetries)) {
+					logger.warn("Reconnect failed, will try again soon: " + e);
+					i++;
+					try {
+						Thread.sleep(wait);
+					} catch (InterruptedException e1) {
+						logger.error(e1, e1);
+					}
+				} else {
+					throw new RuntimeException(e);
+				}
 			}
-			Class<DatabaseAdapter> clazz= (Class<DatabaseAdapter>) Class.forName(adapter);
-			Constructor<DatabaseAdapter> ctor = clazz.getConstructor(Properties.class);
-			return ctor.newInstance(connInfo);
-		} catch (Exception e) {
-			logger.error(e, e);
-			return null;
 		}
 	}
 }
