1cc80196f2e0b40ca24c0890646abea6abcdb24e
hendrik <nhnb@users.sourceforge.net>
2012-03-26 22:25:47 +0200

    don't call new String("string")

--- tests/marauroa/common/game/RPObjectIDTest.java
+++ tests/marauroa/common/game/RPObjectIDTest.java
@@ -24,7 +24,7 @@ public class RPObjectIDTest {
 		RPObject.ID id3  = new RPObject.ID(13 , new String("testzone"));
 		assertEquals(id3.getObjectID(),13);
 		assertEquals(id3.hashCode(),13);
-		
+
 	}
 
 	@Test
@@ -55,9 +55,9 @@ public class RPObjectIDTest {
 
 		RPObject withzone = new RPObject(initID);
 		assertEquals(initID, withzone.getID());
-		
-		
-		
+
+
+
 		RPObject.ID initID2  = new RPObject.ID(idNumber , (String) null);
 
 		RPObject withoutZone = new RPObject(initID2);
@@ -69,14 +69,14 @@ public class RPObjectIDTest {
 		RPAction action = new RPAction();
 		action.put("sourceid", 1);
 		action.put("zoneid", "testzone");
-		
+
 		RPObject.ID withzone = new RPObject.ID(action);
 		RPObject.ID initID = new RPObject.ID(1,"testzone");
 		assertEquals(initID , withzone);
 		action.remove("zoneid");
-		
+
 		RPObject.ID initID2 = new RPObject.ID(1,(String) null);
-	
+
 		RPObject.ID withoutZone = new RPObject.ID(action);
 		assertFalse(initID2.equals(withzone));
 		assertEquals(initID2, withoutZone);
@@ -84,51 +84,51 @@ public class RPObjectIDTest {
 
 	@Test
 	public void testGetObjectID() {
-		RPObject.ID id3  = new RPObject.ID(0 , new String("testzone"));
+		RPObject.ID id3  = new RPObject.ID(0 , "testzone");
 		assertEquals(id3.getObjectID(),0);
 	}
 
 	@Test
 	public void testGetZoneID() {
-		RPObject.ID id3  = new RPObject.ID(0 , new String("testzone"));
+		RPObject.ID id3  = new RPObject.ID(0 , "testzone");
 		assertEquals(id3.getZoneID(), "testzone");
 
 	}
 
 	@Test
 	public void testEqualsObject() {
-		RPObject.ID id3  = new RPObject.ID(0 , new String("testzone"));
-		RPObject.ID id4  = new RPObject.ID(0 , new String("testzone"));
-		
+		RPObject.ID id3  = new RPObject.ID(0 , "testzone");
+		RPObject.ID id4  = new RPObject.ID(0 , "testzone");
+
 		assertTrue(id3.equals(id4));
 		assertTrue(id4.equals(id3));
-		RPObject.ID id5  = new RPObject.ID(1 , new String("testzone"));
+		RPObject.ID id5  = new RPObject.ID(1 , "testzone");
 		assertFalse(id3.equals(id5));
 		assertFalse(id4.equals(id5));
 		assertFalse(id5.equals(id4));
 		assertFalse(id5.equals(id3));
-		
+
 		RPObject.ID id  = new RPObject.ID(0 , (String) null);
 		RPObject.ID id2  = new RPObject.ID(0 , (String) null);
-		
+
 		assertTrue(id.equals(id));
 		assertTrue(id2.equals(id));
-	
+
 		RPObject.ID id6  = new RPObject.ID(0 , "notnull");
-		
+
 		assertFalse(id.equals(id6));
 		assertFalse(id6.equals(id));
-		
+
 	}
 
 	@Test
 	public void testToString() {
 		RPObject.ID id6  = new RPObject.ID(0 , "notnull");
 		assertEquals("RPObject.ID [id=0 zoneid=notnull]", id6.toString());
-		
+
 		RPObject.ID id2  = new RPObject.ID(0 , (String) null);
 		assertEquals("RPObject.ID [id=0 zoneid=null]", id2.toString());
-		
+
 	}
 
 }
