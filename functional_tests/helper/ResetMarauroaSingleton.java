e3e16235c3ab5fa972364539818aba1f1595a5b7
hendrik <nhnb@users.sourceforge.net>
2012-04-21 19:06:57 +0200

    on marauroa reset, clear Configuration object

--- functional_tests/helper/ResetMarauroaSingleton.java
+++ functional_tests/helper/ResetMarauroaSingleton.java
@@ -2,20 +2,17 @@ package helper;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import javax.management.InstanceNotFoundException;
 import javax.management.MBeanRegistrationException;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
+import marauroa.common.Configuration;
+import marauroa.common.ConfigurationParams;
 import marauroa.common.net.NetConst;
 import marauroa.server.marauroad;
 import marauroa.server.game.Statistics;
-
-import org.junit.BeforeClass;
-import org.python.modules.synchronize;
 
 
 /**
@@ -24,40 +21,46 @@ import org.python.modules.synchronize;
  *
  */
 public class ResetMarauroaSingleton {
-	
+
 	public static void resetMarauroa() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
 	Field field = marauroad.class.getDeclaredField("marauroa");
 		field.setAccessible(true);
 		field.set(null, null);
 		field.setAccessible(false);
-		
+
 		field = Statistics.class.getDeclaredField("stats");
 		field.setAccessible(true);
 		field.set(null, null);
 		field.setAccessible(false);
-		
-		field = NetConst.class.getDeclaredField("tcpPort");
-			field.setAccessible(true);
-			   Field modifiersField = Field.class.getDeclaredField("modifiers");
-			    modifiersField.setAccessible(true);
-			    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-			field.setInt(null, 3214);
-			field.setAccessible(false);
-			
-			
-			try {
-				ManagementFactory.getPlatformMBeanServer().unregisterMBean(new ObjectName("marauroad:name=Statistics"));
-			} catch (MBeanRegistrationException e) {
-				e.printStackTrace();
-			} catch (InstanceNotFoundException e) {
-				e.printStackTrace();
-			} catch (MalformedObjectNameException e) {
-				e.printStackTrace();
-			} catch (NullPointerException e) {
-				e.printStackTrace();
-			}
-		
+		field = NetConst.class.getDeclaredField("tcpPort");
+		field.setAccessible(true);
+		   Field modifiersField = Field.class.getDeclaredField("modifiers");
+		    modifiersField.setAccessible(true);
+		    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+		field.setInt(null, 3214);
+		field.setAccessible(false);
+
+
+		try {
+			ManagementFactory.getPlatformMBeanServer().unregisterMBean(new ObjectName("marauroad:name=Statistics"));
+		} catch (MBeanRegistrationException e) {
+			e.printStackTrace();
+		} catch (InstanceNotFoundException e) {
+			e.printStackTrace();
+		} catch (MalformedObjectNameException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		field = Configuration.class.getDeclaredField("staticParams");
+		field.setAccessible(true);
+		field.set(null, new ConfigurationParams());
+		field = Configuration.class.getDeclaredField("configuration");
+		field.setAccessible(true);
+		field.set(null, null);
 	}
 
 	public static void sysoutthreads() throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
