ba8b1d56ee67afd875b47d3d492351442e25f2c8
Astrid Sawatzky <astridemma@users.sourceforge.net>
2012-04-21 17:01:02 +0200

    cleaned up old functional tests

--- functional_tests/marauroa/functional/IFunctionalTest.java
+++
@@ -1,7 +0,0 @@
-package marauroa.functional;
-
-public interface IFunctionalTest {
-	public void setUp() throws Exception;
-	public void launch() throws Exception;
-	public void tearDown() throws Exception;
-}
