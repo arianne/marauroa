17cafa55b73fe39d970d3fb2f702a5f29451b5a8
hendrik <nhnb@users.sourceforge.net>
2013-11-19 21:59:00 +0100

    added method to translate a key using the thread language

--- src/marauroa/common/I18N.java
+++ src/marauroa/common/I18N.java
@@ -1,6 +1,8 @@
 package marauroa.common;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * internationalization support
@@ -9,6 +11,7 @@ import java.util.Locale;
  */
 public class I18N {
 	private static ThreadLocal<Locale> threadLocale = new ThreadLocal<Locale>();
+	private static Map<String, Map<String, String>> dictionaries = new HashMap<String, Map<String, String>>();
 
 	/**
 	 * sets the locale for this thread
@@ -39,4 +42,23 @@ public class I18N {
 		return res;
 	}
 
+	/**
+	 * translates the text to the thread language
+	 *
+	 * @param key  text to translate
+	 * @param args optional arguments
+	 * @return     translated text
+	 */
+	public static String _(String key, Object... args) {
+		Locale locale = getLocale();
+		String value = null;
+		Map<String, String> dictionary = dictionaries.get(locale.getLanguage());
+		if (dictionary != null) {
+			value = dictionary.get(key);
+		}
+		if (value == null) {
+			value = key;
+		}
+		return String.format(locale, value, args);
+	}
 }
