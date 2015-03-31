0c5925f58a9f7a0bb8742191be496bd6e0cbab19
Jordan Irwin <antumdeluge@gmail.com>
2015-03-30 21:29:18 -0700

    Marauroa 3.9.2 -> 3.9.3.

--- extras/marauroad.bat
+++ extras/marauroad.bat
@@ -1,2 +1,2 @@
-set LOCALCLASSPATH=.;marauroa-3.9.2.jar;libs/jython.jar;libs/h2.jar;libs/mysql-connector.jar;libs/log4j.jar
+set LOCALCLASSPATH=.;marauroa-3.9.3.jar;libs/jython.jar;libs/h2.jar;libs/mysql-connector.jar;libs/log4j.jar
 java -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c server.ini -l
