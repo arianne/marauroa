540a5612147dc6ad3c0149b29459dab88606d184
Hendrik Brummermann <nhnb@users.sourceforge.net>
2015-12-31 13:25:37 +0100

    added file header

--- tests/marauroa/server/db/adapter/CreateIndexStatementParserTest.java
+++ tests/marauroa/server/db/adapter/CreateIndexStatementParserTest.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                   (C) Copyright 2015-2016 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package marauroa.server.db.adapter;
 
 import static org.hamcrest.CoreMatchers.equalTo;
