fbb13bee560cf505dc22cd514ea58523ae9aef9c
Hendrik Brummermann <nhnb@users.sourceforge.net>
2015-12-31 13:23:03 +0100

    parser for CREATE INDEX statements

---
+++ src/marauroa/server/db/adapter/CreateIndexStatementParser.java
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *                   (C) Copyright 2015-2016 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package marauroa.server.db.adapter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * parses CREATE INDEX statements
+ *
+ * @author hendrik
+ */
+class CreateIndexStatementParser {
+	private boolean unique;
+	private String name;
+	private String table;
+	private String columns;
+
+	public CreateIndexStatementParser(String sql) {
+		String pattern = "(?i)"
+			+ "create[ ]*((?:unique)?)[ ]+index [ ]*" // CREATE [UNIQUE] INDEX
+			+ "(?:if[ ]+not[ ]+exists )?[ ]*"         // IF NOT EXISTS
+			+ "([^ ]+)[ ]*on[ ]+"                     // name ON
+			+ "([^ (]+)[ ]*"                          // table
+			+ "(\\([^)]*\\));?";                      // (column,...)
+
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(sql);
+
+		if (!m.matches()) {
+			throw new IllegalArgumentException("Failed to parse CREATE INDEX statement: " + sql);
+		}
+
+		unique = !m.group(1).equals("");
+		name = m.group(2);
+		table = m.group(3);
+		columns = m.group(4);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("CREATE ");
+		if (unique) {
+			sb.append("UNIQUE ");
+		}
+		sb.append("INDEX ");
+		sb.append(name);
+		sb.append(" ON ");
+		sb.append(table);
+		sb.append(columns);
+		return sb.toString();
+	}
+}
---
+++ tests/marauroa/server/db/adapter/CreateIndexStatementParserTest.java
@@ -0,0 +1,72 @@
+package marauroa.server.db.adapter;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Tests for CreateIndexStatementParser
+ *
+ * @author hendrik
+ */
+@RunWith(Parameterized.class)
+public class CreateIndexStatementParserTest {
+
+	/**
+	 * generates data for tests
+	 *
+	 * @return data
+	 */
+	@Parameters
+	public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+        	{
+        		"CREATE INDEX IF NOT EXISTS i_gameEvents_event_timedate ON gameEvents(event, timedate);",
+        		"CREATE INDEX i_gameEvents_event_timedate ON gameEvents(event, timedate)"
+        	},
+        	{
+        		"CREATE UNIQUE INDEX IF NOT EXISTS i_gameEvents_event_timedate ON gameEvents(event, timedate);",
+        		"CREATE UNIQUE INDEX i_gameEvents_event_timedate ON gameEvents(event, timedate)"
+        	},
+        	{
+        		"CREATE   INDEX   IF   NOT   EXISTS   i_gameEvents_event_timedate   ON   gameEvents  (event, timedate);",
+        		"CREATE INDEX i_gameEvents_event_timedate ON gameEvents(event, timedate)"
+        	},
+        	{
+        		"CREATE   UNIQUE   INDEX   IF   NOT   EXISTS   i_gameEvents_event_timedate   ON   gameEvents  (event, timedate);",
+        		"CREATE UNIQUE INDEX i_gameEvents_event_timedate ON gameEvents(event, timedate)"
+        	}
+        });
+	}
+
+	private String input;
+	private String output;
+
+	/**
+	 * Tests for CreateIndexStatementParser
+	 *
+	 * @param input input test data
+	 * @param output expected output
+	 */
+	public CreateIndexStatementParserTest(String input, String output) {
+		this.input = input;
+		this.output = output;
+	}
+
+	/**
+	 * tests for toString()
+	 */
+	@Test
+	public void testToString() {
+		CreateIndexStatementParser parser = new CreateIndexStatementParser(input);
+		assertThat(parser.toString(), equalTo(output));
+	}
+
+}
