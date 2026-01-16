6d20848b2b1a2ae3704f73dc58310dc37d0016db
Hendrik Brummermann <nhnb@users.sourceforge.net>
2026-01-16 21:35:57 +0100

    use maps for factories

--- src/js/rpfactory.js
+++ src/js/rpfactory.js
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2011-2017 - Marauroa                    *
+ *                   (C) Copyright 2011-2026 - Marauroa                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -14,14 +14,10 @@
 
 var marauroa = window.marauroa = window.marauroa || {};
 
-/**marauroa.rpobjectFactory
- * creates RPObjects
- *
- * use the rpclass name as attribute name for a prototype object
- */
-marauroa.rpobjectFactory = new function(){
-	this["_default"] = {};
-	this["_default"].onEvent = function(e) {
+(function() {
+
+let defaultRPObject = {
+	onEvent: function(e) {
 		var event = marauroa.rpeventFactory.create(e["c"]);
 		for (var i in e["a"]) {
 			if (e["a"].hasOwnProperty(i)) {
@@ -36,45 +32,76 @@ marauroa.rpobjectFactory = new function(){
 			}
 		}
 		event.execute(this);
-	}
-	this["_default"].set = function(key, value) {
+	},
+	set: function(key, value) {
 		this[key] = value;
-	}
-	this["_default"].setMapEntry = function(map, key, value) {
+	},
+	setMapEntry: function(map, key, value) {
 		this[map][key] = value;
-	}
-	this["_default"].unset = function(key) {
+	},
+	unset: function(key) {
 		delete this[key];
-	}
-	this["_default"].unsetMapEntry = function(map, key) {
+	},
+	unsetMapEntry: function(map, key) {
 		delete this[map][key];
-	}
-	this["_default"].destroy = function(parent) {
+	},
+	destroy: function(parent) {
 		// do nothing
-	}
-	this["_default"].createSlot = function(name) {
+	},
+	createSlot: function(name) {
 		var slot = marauroa.rpslotFactory.create(name);
 		slot._parent = this;
 		return slot;
-	}
+	},
 
-	this["_default"].init = function() {
+	init: function() {
 		// do nothing
+	}
+	
+}
+
+let rpClasses = new Map();
+
+/**
+ * marauroa.rpobjectFactory
+ * creates RPObjects
+ *
+ * use the rpclass name as attribute name for a prototype object
+ */
+marauroa.rpobjectFactory = new function() {
+
+	this.register = function(name, clazz) {
+		rpClasses.set(name, clazz);
 	}
 
 	this.create = function(rpclass) {
-		var ctor = marauroa.rpobjectFactory["_default"];
-		if (typeof(marauroa.rpobjectFactory[rpclass]) != "undefined") {
-			ctor = marauroa.rpobjectFactory[rpclass];
+		let ctor = rpClasses.get(rpclass);
+		if (!ctor) {
+			if (ctor) {
+				console.log("Deprecated use of marauroa.rpobjectFactory[name]. Please use marauroa.rpobjectFactory.register() instead.")
+			} else {
+				ctor = rpClasses.get("_default");
+			}
 		}
-		var temp = marauroa.util.fromProto(ctor);
+		let temp = marauroa.util.fromProto(ctor);
 		temp.init();
 		return temp;
 	};
+
+	rpClasses.set("_default", defaultRPObject);
 }
 
 
 
+let defaultRPEvent = {
+	execute: function(rpobject) {
+		if (marauroa.debug.unknownEvents) {
+			console.log("Unhandled event: ", this, " on ", rpobject);
+		}
+	}
+}
+
+let rpEvents = new Map();
 
 /**
  * creates RPEvent
@@ -82,52 +109,52 @@ marauroa.rpobjectFactory = new function(){
  * use the rpclass name as attribute name for a prototype object
  */
 marauroa.rpeventFactory = new function(){
-	this["_default"] = {};
-	this["_default"].execute = function(rpobject) {
-		if (marauroa.debug.unknownEvents) {
-			console.log("Unhandled event: ", this, " on ", rpobject);
-		}
+
+	this.register = function(name, clazz) {
+		rpEvents.set(name, clazz);
 	}
 
 	this.create = function(rpclass) {
-		var ctor = marauroa.rpeventFactory["_default"];
-		if (typeof(marauroa.rpeventFactory[rpclass]) != "undefined") {
-			ctor = marauroa.rpeventFactory[rpclass];
+		let ctor = rpEvents.get(rpclass);
+		if (!ctor) {
+			if (ctor) {
+				console.log("Deprecated use of marauroa.rpeventFactory[name]. Please use marauroa.rpeventFactory.register() instead.")
+			} else {
+				ctor = rpEvents.get["_default"];
+			}
 		}
 		return marauroa.util.fromProto(ctor);
 	}
+
+	rpEvents.set("_default", defaultRPEvent);
 }
 
+let defaultRPSlot = {
+	_objects: [],
 
-
-/**
- * creates RPSlot
- *
- * use the rpclass name as attribute name for a prototype object
- */
-marauroa.rpslotFactory = new function(){
-	this["_default"] = {
-		_objects: []
-	};
-	this["_default"].add = function(value) {
+	add: function(value) {
 		if (value && value["id"]) {
 			this._objects.push(value);
 		}
-	}
-	this["_default"].get = function(key) {
+	},
+
+	get: function(key) {
 		var idx = this.getIndex(key);
 		if (idx > -1) {
 			return this._objects[idx];
 		}
 		return undefined;
-	}
-	this["_default"].getByIndex = function(idx) {
+	},
+
+	getByIndex: function(idx) {
 		return this._objects[idx];
-	}
-	this["_default"].count = function() {
+	},
+
+	count: function() {
 		return this._objects.length;
-	}
-	this["_default"].getIndex = function(key) {
+	},
+
+	getIndex: function(key) {
 		var i;
 		var c = this._objects.length;
 		for (i = 0; i < c; i++) {
@@ -136,28 +163,53 @@ marauroa.rpslotFactory = new function(){
 			}
 		}
 		return -1;
-	}
-	this["_default"].del = function(key) {
+	},
+
+	del: function(key) {
 		var idx = this.getIndex(key);
 		if (idx > -1) {
 			this._objects.splice(idx, 1);
 		}
-	}
-	this["_default"].first = function() {
+	},
+
+	first: function() {
 		if (this._objects.length > 0) {
 			return this._objects[0];
 		}
 		return undefined;
 	}
+}
+
+let rpSlots = new Map();
+
+/**
+ * creates RPSlot
+ *
+ * use the rpclass name as attribute name for a prototype object
+ */
+marauroa.rpslotFactory = new function(){
+
+	this.register = function(name, clazz) {
+		rpSlots.set(name, clazz);
+	}
 
 	this.create = function(name) {
-		var ctor = marauroa.rpslotFactory["_default"];
-		if (typeof(marauroa.rpslotFactory[name]) != "undefined") {
+		let ctor = rpSlots.get(name);
+		if (!ctor) {
 			ctor = marauroa.rpslotFactory[name];
+			if (ctor) {
+				console.log("Deprecated use of marauroa.rpslotFactory[name]. Please use marauroa.rpslotFactory.register() instead.")
+			} else {
+				ctor = rpSlots.get("_default");
+			}
 		}
-		var slot = marauroa.util.fromProto(ctor);
+		let slot = marauroa.util.fromProto(ctor);
 		slot._name = name;
 		slot._objects = [];
 		return slot;
 	};
+
+	rpSlots.set("_default", defaultRPSlot);
 };
+
+})();
