46a96b111db00499b28b8aa41bf328399b34cdcb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2026-01-17 15:03:48 +0100

    started to define API

---
+++ src/js/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ **************************************************************************
+ *                   (C) Copyright 2011-2026 - Marauroa                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+export { marauroa } from "./Marauroa";
+export { RPObject } from "./RPObject";
+export { RPEvent } from "./RPEvent";
+export { RPSlot } from "./RPSlot";
+export { ClientFramework } from "./ClientFramework";
